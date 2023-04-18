e9b7119da72b224e7e42a7f56dc6327eb54f0f68
Denis Davydkov <denis@davydkov.com>
2023-04-18 10:31:22 +0200

    update package.jsons

--- packages/diagrams/src/theme/index.ts
+++ packages/diagrams/src/theme/index.ts
@@ -1,2 +1,1 @@
-export * from './sprinkles.css'
 export * from './vars.css'
--- packages/vscode/esbuild.mjs
+++ packages/vscode/esbuild.mjs
@@ -1,53 +1,74 @@
 import * as esbuild from 'esbuild'
 import { formatMessagesSync } from 'esbuild'
+import { writeFile } from 'node:fs/promises'
+import path from 'node:path'
 
+const minify = process.argv.includes('--minify')
 const watch = process.argv.includes('--watch')
+
+const alias = {
+  'vscode-uri': '../../node_modules/vscode-uri/lib/esm/index.js',
+  'vscode-languageserver-types': '../../node_modules/vscode-languageserver-types/lib/esm/main.js',
+  'vscode-languageserver-textdocument': '../../node_modules/vscode-languageserver-textdocument/lib/esm/main.js'
+}
+
+console.log('alias', alias)
 
 /**
  * @type {esbuild.BuildOptions}
  */
 const nodeCfg = {
-  entryPoints: {
-    index: 'src/extension/node/index.ts',
-    server: 'src/extension/node/server.ts'
-  },
+  entryPoints: [
+    'src/extension/node/index.ts',
+    'src/extension/node/server.ts'
+  ],
+  metafile: true,
   logLevel: 'info',
-  outdir: 'dist/node',
+  outbase: 'src/extension',
+  outdir: 'dist',
+  conditions: ['module','import'],
   bundle: true,
   external: ['vscode'],
   format: 'cjs',
   target: 'node16',
   platform: 'node',
+  alias: {
+    ...alias
+  },
   color: true,
   allowOverwrite: true,
-  sourcemap: 'external',
-  sourcesContent: true,
-  treeShaking: true
+  sourcemap: true,
+  sourcesContent: false,
+  minify
 }
 
 /**
  * @type {esbuild.BuildOptions}
  */
 const webCfg = {
-  entryPoints: {
-    index: 'src/extension/browser/index.ts',
-    server: 'src/extension/browser/server.ts'
-  },
+  entryPoints: [
+    'src/extension/browser/index.ts',
+    'src/extension/browser/server.ts'
+  ],
+  metafile: true,
   logLevel: 'info',
-  outdir: 'dist/browser',
+  outbase: 'src/extension',
+  outdir: 'dist',
   bundle: true,
   format: 'cjs',
+  target: 'es2022',
   platform: 'browser',
   external: ['vscode'],
   mainFields: ['browser', 'module', 'main'],
   alias: {
-    path: 'path-browserify'
+    path: 'path-browserify',
+    ...alias
   },
   color: true,
   allowOverwrite: true,
-  sourcemap: 'external',
-  sourcesContent: true,
-  treeShaking: true
+  sourcemap: true,
+  sourcesContent: false,
+  minify
 }
 
 if (!watch) {
@@ -55,6 +76,17 @@ if (!watch) {
     esbuild.build(nodeCfg),
     esbuild.build(webCfg)
   ])
+
+  // const [nodeBundle, webBundle] = bundles
+  // if (nodeBundle.metafile) {
+  //   const metafile = path.resolve('dist', 'node', 'metafile.json')
+  //   await writeFile(metafile, JSON.stringify(nodeBundle.metafile))
+  // }
+  // if (webBundle.metafile) {
+  //   const metafile = path.resolve('dist', 'browser', 'metafile.json')
+  //   await writeFile(metafile, JSON.stringify(webBundle.metafile))
+  // }
+
   const errors = bundles.flatMap(b => b.errors)
   const warnings = bundles.flatMap(b => b.warnings)
   if (errors.length || warnings.length) {
