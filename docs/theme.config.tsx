f2802bd1ec124008fe764750aafcc9b31153aba7
Denis Davydkov <denis@davydkov.com>
2023-05-04 11:16:31 +0200

    use Callout/Cards from nextra-theme-docs

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,41 +1,38 @@
 import React from 'react'
 import type { DocsThemeConfig} from 'nextra-theme-docs';
-import { useConfig } from 'nextra-theme-docs'
+import { useConfig} from 'nextra-theme-docs';
 import { Logo } from '$/components/logo'
 
 const config: DocsThemeConfig = {
   logo: <Logo height={30} textFill='#FFF' />,
   darkMode: true,
   primaryHue: 195,
-  useNextSeoProps() {
-    const { frontMatter } = useConfig()
-    if (frontMatter.title) {
-      return {
-        title: frontMatter.title,
-        // description: frontMatter.description,
-        // openGraph: {
-        //   title: frontMatter.title,
-        //   description: frontMatter.description,
-        //   type: 'website',
-        //   site_name: 'LikeC4'
-        // }
-      }
-    }
-    return {
-      titleTemplate: '%s – LikeC4'
-    }
+  sidebar: {
+    toggleButton: true,
   },
   //main: ({ children }) => <main className='asd'>{children}</main>,
   project: {
     link: 'https://github.com/likec4/likec4',
   },
   docsRepositoryBase: 'https://github.com/likec4/likec4/blob/main/docs',
-  // footer: {
-  //   text: 'Nextra Docs Template',
-  // },
   themeSwitch: {
     component: () => null
-  }
+  },
+  footer: {
+    text: `MIT ${new Date().getFullYear()} LikeC4`
+  },
+  useNextSeoProps: () => {
+    const { frontMatter } = useConfig()
+    if (frontMatter.title) {
+      return {
+        title: frontMatter.title,
+      }
+    }
+    return {
+      titleTemplate: '%s – LikeC4'
+    }
+  },
+  // useNextSeoProps: () => ({ titleTemplate: '%s – LikeC4' })
 }
 
 export default config
