fec3fc75a8eade98d142fe123f1a225053cf7ac0
Denis Davydkov <denis@davydkov.com>
2023-05-04 10:54:04 +0200

    rename vitest.shared.mjs to vitest.shared.ts

--- packages/language-server/vitest.config.ts
+++ packages/language-server/vitest.config.ts
@@ -1,5 +1,5 @@
 import { mergeConfig, defineProject } from 'vitest/config'
-import configShared from '../../vitest.shared.mjs'
+import configShared from '../../vitest.shared'
 
 export default mergeConfig(
   configShared,
