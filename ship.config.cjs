1deec9159db470ffd7c464450775b70cb1d8d3e7
Denis Davydkov <denis@davydkov.com>
2023-06-24 00:49:12 +0200

    try shipjs

--- ship.config.cjs
+++ ship.config.cjs
@@ -2,6 +2,17 @@ module.exports = {
   monorepo: {
     mainVersionFile: 'package.json', // or `lerna.json`, or whatever a json file you can read the latest `version` from.
     packagesToBump: ['packages/*', 'docs', 'examples/*'],
-    packagesToPublish: ['packages/*'],
+    packagesToPublish: [
+      'packages/core',
+      'packages/cli',
+      'packages/diagrams',
+      'packages/generators',
+      'packages/language-server',
+      'packages/layouts',
+    ],
   },
+  updateChangelog: false,
+  draftPullRequest: true,
+  publishCommand: ({ defaultCommand }) =>
+    `${defaultCommand} --access public`
 };
