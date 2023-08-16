d2407cc2b2b1a579deb0e27892b3cf2a6583ee6c
Denis Davydkov <denis@davydkov.com>
2023-08-17 01:44:12 +0200

    Revert "chore: expermint with unbuld"
    This reverts commit 6f1981eeb09b47da40b69128df8bf7aa1fe54185.

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -1,59 +1,25 @@
 /* eslint-disable */
 import { defineBuildConfig } from 'unbuild'
-import type { MkdistOptions } from 'mkdist'
 
-const opts = {
-  pattern: ['**/*.ts', '!**/*.spec.ts', '!__test__/'],
-  esbuild: {
-    platform: 'neutral'
-  }
-} satisfies MkdistOptions
+const pattern = ['**/*.ts', '!**/*.spec.ts', '!__test__/']
 
 // prettier-ignore
 export default defineBuildConfig([{
-//   entries: [
-//     'src/compute-view/index.ts',
-//     'src/types/index.ts',
-//   ],
-//   clean: true,
-//   failOnWarn: false,
-//   sourcemap: true,
-//   declaration: true,
-//   rollup: {
-//     emitCJS: true,
-//   }
-// },{
-  entries: [
-    'src/types/index.ts',
-    'src/colors.ts',
-    'src/errors/index.ts',
-    'src/utils/index.ts',
-    'src/compute-view/index.ts',
-    'src/index.ts',
-  ],
-  clean: true,
-  // failOnWarn: false,
-  // sourcemap: true,
-  declaration: 'node16',
-  rollup: {
-    emitCJS: true,
-  },
-
-  // entries: [{
-  //   builder: 'mkdist',
-  //   input: 'src',
-  //   format: 'esm',
-  //   ext: 'js',
-  //   ...opts,
-  // },{
-  //   builder: 'mkdist',
-  //   input: 'src',
-  //   format: 'cjs',
-  //   ext: 'cjs' as any,
-  //   ...opts,
-  // }],
+  entries: [{
+    builder: 'mkdist',
+    input: 'src',
+    format: 'esm',
+    ext: 'js',
+    pattern,
+  },{
+    builder: 'mkdist',
+    input: 'src',
+    format: 'cjs',
+    ext: 'cjs' as any,
+    pattern
+  }],
   // if clean enabled, TS Language server in VSCode has to be restarted
-  // clean: false,
-  // sourcemap: true,
-  // declaration: 'compatible',
+  clean: false,
+  sourcemap: true,
+  declaration: 'compatible',
 }])
