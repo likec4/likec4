5ee1bc5ff3bdd79383f94d37c591d54515444dd0
Denis Davydkov <denis@davydkov.com>
2023-08-20 07:24:29 +0200

    chore: align with protocol changes

--- docs/components/playground-page/editor/protocol.ts
+++ docs/components/playground-page/editor/protocol.ts
@@ -1,5 +1,5 @@
 import type { Fqn, LikeC4Model, RelationID, ViewID } from '@likec4/core'
-import type { DocumentUri, Location, } from 'vscode-languageclient/lib/common/api'
+import type { DocumentUri, Location } from 'vscode-languageclient/lib/common/api'
 import { NotificationType, RequestType0, RequestType } from 'vscode-languageclient/lib/common/api'
 
 //#region From server
@@ -14,29 +14,28 @@ interface BuildDocumentsParams {
 }
 const buildDocuments = new RequestType<BuildDocumentsParams, void, void>('likec4/buildDocuments')
 
-interface LocateElementParams {
-  element: Fqn
-  property?: string
-}
-const locateElement = new RequestType<
-  LocateElementParams,
-  Location | null,
-  void
->('likec4/locateElement')
-
-const locateRelation = new RequestType<{ id: RelationID }, Location | null, void>(
-  'likec4/locateRelation'
-)
-const locateView = new RequestType<{ id: ViewID }, Location | null, void>(
-  'likec4/locateView'
-)
-//#endregion
+export type LocateParams =
+  | {
+      element: Fqn
+      property?: string
+      relation?: never
+      view?: never
+    }
+  | {
+      relation: RelationID
+      element?: never
+      view?: never
+    }
+  | {
+      view: ViewID
+      relation?: never
+      element?: never
+    }
+const locate = new RequestType<LocateParams, Location | null, void>('likec4/locate')
 
 export const Rpc = {
   onDidChangeModel,
   fetchModel,
   buildDocuments,
-  locateElement,
-  locateRelation,
-  locateView
+  locate
 } as const
