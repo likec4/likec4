001b26baef1d83d954a93974ca4d533b91db4efd
Denis Davydkov <denis@davydkov.com>
2023-10-12 02:11:42 +0200

    ci: use localhost for lostpixel

--- examples/diagrams-dev/lostpixel.config.ts
+++ examples/diagrams-dev/lostpixel.config.ts
@@ -1,12 +1,11 @@
 import type { CustomProjectConfig } from 'lost-pixel'
-import { isCI } from 'ci-info'
 
 export const config: CustomProjectConfig = {
   ladleShots: {
     // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
-    ladleUrl: `http://${isCI ? '172.17.0.1' : 'localhost'}:61000`
+    // ladleUrl: `http://${isCI ? '172.17.0.1' : 'localhost'}:61000`
+    ladleUrl: `http://localhost:61000`
   },
-  browser: 'chromium',
   // OSS mode
   generateOnly: true,
   failOnDifference: true
