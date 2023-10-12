e994b882c33db2047b75f47ca8714dd14f2785c3
Denis Davydkov <denis@davydkov.com>
2023-10-12 03:05:28 +0200

    chore: set path explicitly

--- examples/diagrams-dev/lostpixel.config.ts
+++ examples/diagrams-dev/lostpixel.config.ts
@@ -1,11 +1,15 @@
 import type { CustomProjectConfig } from 'lost-pixel'
 import { isCI } from 'ci-info'
+import { resolve } from 'node:path'
 
 export const config: CustomProjectConfig = {
   ladleShots: {
     // IP should be localhost when running locally & 172.17.0.1 when running in GitHub action
     ladleUrl: `http://${isCI ? '172.17.0.1' : 'localhost'}:61000`
   },
+  imagePathBaseline: resolve(__dirname, '.lostpixel/baseline'),
+  imagePathCurrent: resolve(__dirname, '.lostpixel/current'),
+  imagePathDifference: resolve(__dirname, '.lostpixel/difference'),
   // OSS mode
   generateOnly: true,
   failOnDifference: true
