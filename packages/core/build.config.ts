44de100f1fc8279f8aec13fa4162dcd638d5dc18
Denis Davydkov <denis@davydkov.com>
2023-10-15 02:08:04 +0200

    build: use ts references

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -37,7 +37,7 @@ export default defineBuildConfig({
   sourcemap: true,
   hooks: {
     'build:before': async (ctx) => {
-      await $`tsc --emitDeclarationOnly --declaration --declarationMap --listFiles --outDir ./dist/types`
+      await $`tsc --emitDeclarationOnly --declaration --declarationMap --listFiles`
     }
   }
 })
