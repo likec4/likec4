ef7cf93bbb7a397a2685b7952b4a8c601656ab81
Denis Davydkov <denis@davydkov.com>
2023-10-25 03:11:54 +0200

    feat: `npm create likec4`

---
+++ packages/create-likec4/README.md
@@ -0,0 +1,26 @@
+# create-likec4 <a href="https://npmjs.com/package/create-likec4"><img src="https://img.shields.io/npm/v/create-likec4" alt="npm package"></a>
+
+## Scaffolding Your First LikeC4 Project
+
+> **Compatibility Note:**
+> LikeC4 requires [Node.js](https://nodejs.org/en/) version 18+, 20+
+
+With NPM:
+
+```bash
+$ npm create likec4@latest
+```
+
+With Yarn:
+
+```bash
+$ yarn create likec4
+```
+
+With PNPM:
+
+```bash
+$ pnpm create likec4
+```
+
+Then follow the prompts!
--- packages/create-likec4/src/index.ts
+++ packages/create-likec4/src/index.ts
@@ -1,1 +1,98 @@
-console.log('soon')
+#!/usr/bin/env node
+import fs from 'node:fs'
+import { mkdir } from 'fs/promises'
+import { $ } from 'execa'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'path'
+import path from 'node:path'
+import prompts from 'prompts'
+
+function copy(src: string, dest: string) {
+  const stat = fs.statSync(src)
+  if (stat.isDirectory()) {
+    copyDir(src, dest)
+  } else {
+    fs.copyFileSync(src, dest)
+  }
+}
+
+function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, { recursive: true })
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    const destFile = path.resolve(destDir, file)
+    copy(srcFile, destFile)
+  }
+}
+
+function pkgFromUserAgent(userAgent: string | undefined) {
+  if (!userAgent) return undefined
+  const pkgSpec = userAgent.split(' ')[0]
+  if (!pkgSpec) return undefined
+  const pkgSpecArr = pkgSpec.split('/')
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1]
+  }
+}
+
+async function init() {
+  const cwd = process.cwd()
+
+  const pkgInfo = pkgFromUserAgent(process.env['npm_config_user_agent'])
+  const pkgManager = pkgInfo ? pkgInfo.name : 'npm'
+
+  const results = await prompts({
+    type: 'text',
+    name: 'targetDir',
+    initial: '.',
+    message: 'Directory to create project in:'
+  })
+  const root = resolve(cwd, results.targetDir)
+
+  await mkdir(root, { recursive: true })
+
+  // let targetDir = 'likec4'
+
+  console.log(`Creating project in ${root}...`)
+
+  const templateDir = resolve(fileURLToPath(import.meta.url), '../../template')
+
+  const write = (file: string, content?: string) => {
+    const targetPath = path.join(root, file)
+    if (content) {
+      fs.writeFileSync(targetPath, content)
+    } else {
+      copy(path.join(templateDir, file), targetPath)
+    }
+  }
+
+  const files = fs.readdirSync(templateDir)
+  for (const file of files) {
+    write(file)
+  }
+
+  console.log(`\nDone\nInstalling dependencies...`)
+
+  const $$ = $({
+    cwd: root,
+    stderr: 'inherit',
+    stdout: 'inherit'
+  })
+  switch (pkgManager) {
+    case 'yarn': {
+      await $$`yarn`
+      console.log(`\nDone\nRunning dev server...`)
+      await $$`yarn dev`
+      break
+    }
+    default: {
+      await $$`npm install`
+      console.log(`\nDone\nRunning dev server...`)
+      await $$`npm run dev`
+      break
+    }
+  }
+}
+
+await init()
