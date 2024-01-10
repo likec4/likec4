9b4f5253779c47e17747eda239b5e5cf125e440e
Denis Davydkov <denis@davydkov.com>
2024-01-10 22:02:00 +0100

    getBoundingRect around nodes and edges

--- packages/diagrams/src/diagram/utils.ts
+++ packages/diagrams/src/diagram/utils.ts
@@ -1,5 +1,5 @@
 import { is } from 'rambdax'
-import type { KonvaPointerEvent } from './types'
+import type { DiagramEdge, DiagramNode, IRect, KonvaPointerEvent } from './types'
 
 export function mousePointer(e: KonvaPointerEvent) {
   const container = e.target.getStage()?.container()
@@ -16,3 +16,38 @@ export function mouseDefault(e: KonvaPointerEvent) {
 }
 
 export const isNumber = is(Number)
+
+export const getBoundingRect = (elements: (DiagramNode | DiagramEdge)[]): IRect => {
+  let minX = Infinity
+  let minY = Infinity
+  let maxX = -Infinity
+  let maxY = -Infinity
+
+  for (const element of elements) {
+    if ('size' in element && 'position' in element) {
+      minX = Math.min(minX, element.position[0])
+      minY = Math.min(minY, element.position[1])
+      maxX = Math.max(maxX, element.position[0] + element.size.width)
+      maxY = Math.max(maxY, element.position[1] + element.size.height)
+      continue
+    }
+    element.points.forEach(([x, y]) => {
+      minX = Math.min(minX, x)
+      minY = Math.min(minY, y)
+      maxX = Math.max(maxX, x)
+      maxY = Math.max(maxY, y)
+    })
+    if (element.labelBBox) {
+      minX = Math.min(minX, element.labelBBox.x)
+      minY = Math.min(minY, element.labelBBox.y)
+      maxX = Math.max(maxX, element.labelBBox.x + element.labelBBox.width)
+      maxY = Math.max(maxY, element.labelBBox.y + element.labelBBox.height)
+    }
+  }
+  return {
+    x: minX,
+    y: minY,
+    width: maxX - minX,
+    height: maxY - minY
+  }
+}
