4d3637c1a8c3f0694c78c40d87b700f276028e63
Denis Davydkov <denis@davydkov.com>
2024-05-09 05:52:30 +0200

    fix(diagram): zoom out on diagram change

--- packages/diagram/src/xyflow/SyncWithDiagram.tsx
+++ packages/diagram/src/xyflow/SyncWithDiagram.tsx
@@ -1,29 +1,14 @@
 import { deepEqual as eq, shallowEqual } from 'fast-equals'
 import { useXYFlow, useXYStoreApi } from './hooks'
-import type { XYFlowEdge, XYFlowNode } from './types'
+import type { XYFlowNode } from './types'
 
-import { memo, useCallback, useEffect } from 'react'
-import { type DiagramState, useDiagramStoreApi } from '../state'
+import { memo, useEffect } from 'react'
+import { useDiagramStoreApi } from '../state'
 import { diagramViewToXYFlowData } from './diagram-to-xyflow'
 import { toDomPrecision } from './utils'
 
 const isSameNode = (a: XYFlowNode) => (b: XYFlowNode) =>
   a.id === b.id && a.type === b.type && Object.is(a.parentId, b.parentId)
-
-function hasNoChanges(existing: XYFlowNode, update: XYFlowNode) {
-  return existing.id === update.id
-    && existing.type === update.type
-    && existing.parentId == update.parentId
-    && eq(existing.data.element, update.data.element)
-}
-
-function edgeHasNoChanges(existing: XYFlowEdge, update: XYFlowEdge) {
-  return existing.id === update.id
-    && existing.type === update.type
-    && existing.source === update.source
-    && existing.target === update.target
-    && eq(existing.data, update.data)
-}
 
 /**
  * Syncs the diagram state with the XYFlow instance
@@ -32,51 +17,6 @@ export const SyncWithDiagram = memo(() => {
   const xyflow = useXYFlow()
   const xyflowApi = useXYStoreApi()
   const diagramStoreApi = useDiagramStoreApi()
-
-  const correctPosition = useCallback((
-    lastOnNavigate: NonNullable<DiagramState['lastOnNavigate']>,
-    nodes: DiagramState['view']['nodes']
-  ) => {
-    // const {
-    //   lastOnNavigate,
-    //   view: {
-    //     nodes,
-    //     id: viewId
-    //   }
-    // } = diagramStoreApi.getState()
-    // if (lastOnNavigate.toView !== viewId || lastOnNavigate.positionCorrected) {
-    // return
-    // }
-
-    const elFrom = lastOnNavigate.element
-    const elTo = nodes.find(n => n.id === elFrom.id)
-    if (elTo) {
-      const centerFrom = lastOnNavigate.elementCenterScreenPosition,
-        centerTo = xyflow.flowToScreenPosition({
-          x: elTo.position[0] + elTo.width / 2,
-          y: elTo.position[1] + elTo.height / 2
-        }),
-        diff = {
-          x: toDomPrecision(centerFrom.x - centerTo.x),
-          y: toDomPrecision(centerFrom.y - centerTo.y)
-        }
-      requestAnimationFrame(() => {
-        xyflowApi.getState().panBy(diff)
-      })
-    }
-    queueMicrotask(() => {
-      diagramStoreApi.setState(
-        {
-          lastOnNavigate: {
-            ...lastOnNavigate,
-            positionCorrected: true
-          }
-        },
-        false,
-        'positionCorrected'
-      )
-    })
-  }, [diagramStoreApi, xyflowApi])
 
   useEffect(() => {
     return diagramStoreApi.subscribe(
@@ -123,28 +63,30 @@ export const SyncWithDiagram = memo(() => {
         )
 
         if (lastOnNavigate?.toView === viewId && !lastOnNavigate.positionCorrected) {
-          // correctPosition(
-          //   lastOnNavigate,
-          //   nodes
-          // )
           const elFrom = lastOnNavigate.element
           const elTo = nodes.find(n => n.id === elFrom.id)
           if (elTo) {
-            const centerFrom = lastOnNavigate.elementCenterScreenPosition,
-              // xyflow.flowToScreenPosition({
-              //     x: elFrom.position[0] + elFrom.width / 2,
-              //     y: elFrom.position[1] + elFrom.height / 2
-              //   }),
-              centerTo = xyflow.flowToScreenPosition({
-                x: elTo.position[0] + elTo.width / 2,
-                y: elTo.position[1] + elTo.height / 2
-              }),
-              diff = {
-                x: toDomPrecision(centerFrom.x - centerTo.x),
-                y: toDomPrecision(centerFrom.y - centerTo.y)
-              }
+            const centerFrom = lastOnNavigate.elementCenterScreenPosition
+            const nextZoom = Math.min(elFrom.width / elTo.width, elFrom.height / elTo.height)
+
             requestAnimationFrame(() => {
               requestAnimationFrame(() => {
+                const v = xyflow.getViewport()
+                if (nextZoom < v.zoom) {
+                  xyflow.setViewport({
+                    x: v.x,
+                    y: v.y,
+                    zoom: nextZoom
+                  })
+                }
+                const centerTo = xyflow.flowToScreenPosition({
+                    x: elTo.position[0] + elTo.width / 2,
+                    y: elTo.position[1] + elTo.height / 2
+                  }),
+                  diff = {
+                    x: toDomPrecision(centerFrom.x - centerTo.x),
+                    y: toDomPrecision(centerFrom.y - centerTo.y)
+                  }
                 xyflowApi.getState().panBy(diff)
               })
             })
@@ -165,7 +107,7 @@ export const SyncWithDiagram = memo(() => {
         equalityFn: shallowEqual
       }
     )
-  }, [xyflow, xyflowApi, diagramStoreApi, correctPosition])
+  }, [xyflow, xyflowApi, diagramStoreApi])
 
   return null
 })
