edd57f0215f91fb3a221abaf25e3ff46246a255d
Denis Davydkov <denis@davydkov.com>
2024-05-10 05:00:09 +0200

    fix(diagram): remove EmbeddedLikeC4Diagram

--- packages/diagram/src/index.ts
+++ packages/diagram/src/index.ts
@@ -4,7 +4,6 @@ import './globals.css'
 export { LikeC4Diagram } from './LikeC4Diagram'
 export { StaticLikeC4Diagram } from './StaticLikeC4Diagram'
 
-export type * from './EmbeddedLikeC4Diagram'
 export * from './hooks'
 export type * from './LikeC4Diagram'
 export type * from './LikeC4Diagram.props'
