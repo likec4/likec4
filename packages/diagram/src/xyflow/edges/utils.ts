6162cb207c230eb708a2bab9bf35f1cd94cab9dd
Denis Davydkov <denis@davydkov.com>
2024-06-03 22:49:13 +0200

    experiment with edge controls points

--- packages/diagram/src/xyflow/edges/utils.ts
+++ packages/diagram/src/xyflow/edges/utils.ts
@@ -1,6 +1,37 @@
+import type { Point } from '@likec4/core'
 import { Position, type XYPosition } from '@xyflow/react'
 import { getNodeDimensions } from '@xyflow/system'
 import type { InternalXYFlowNode } from '../types'
+
+// this helper function returns the intersection point
+// of the line between the center of the intersectionNode and the target node
+export function getNodeIntersectionFromCenterToPoint(intersectionNode: InternalXYFlowNode, [x1, y1]: Point) {
+  // https://math.stackexchange.com/questions/1724792/an-algorithm-for-finding-the-intersection-point-between-a-center-of-vision-and-a
+  const {
+    width: intersectionNodeWidth,
+    height: intersectionNodeHeight
+  } = getNodeDimensions(intersectionNode)
+
+  const intersectionNodePosition = intersectionNode.internals.positionAbsolute
+
+  const w = intersectionNodeWidth / 2
+  const h = intersectionNodeHeight / 2
+
+  const x2 = intersectionNodePosition.x + w
+  const y2 = intersectionNodePosition.y + h
+  // const x1 = targetPoint.x
+  // const y1 = targetPoint.y
+
+  const xx1 = (x1 - x2) / (2 * w) - (y1 - y2) / (2 * h)
+  const yy1 = (x1 - x2) / (2 * w) + (y1 - y2) / (2 * h)
+  const a = 1 / (Math.abs(xx1) + Math.abs(yy1))
+  const xx3 = a * xx1
+  const yy3 = a * yy1
+  const x = w * (xx3 + yy3) + x2
+  const y = h * (-xx3 + yy3) + y2
+
+  return [x, y] as const
+}
 
 // this helper function returns the intersection point
 // of the line between the center of the intersectionNode and the target node
