6c7d424f3edfc58f58a1080df6cfa4b3fb1f0720
Denis Davydkov <denis@davydkov.com>
2024-06-17 22:31:39 +0200

    chore: reorganize folders and test tas

--- _vitest.workspace.ts
+++ _vitest.workspace.ts
@@ -1,3 +1,6 @@
+/**
+ * Disabled for now, as vitest vscode extension does not work properly
+ */
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace(['packages/*'])
--- packages/likec4/vitest.config.ts
+++ packages/likec4/vitest.config.ts
@@ -2,6 +2,7 @@ import { defineProject } from 'vitest/config'
 
 export default defineProject({
   test: {
+    passWithNoTests: true,
     include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
     name: 'likec4',
     chaiConfig: {
