192e8f8c9e6bbaed790653dfe3b1172b5ede6ac3
Denis Davydkov <denis@davydkov.com>
2024-08-07 04:42:58 +0200

    chore: Update router configuration

--- packages/likec4/app/src/router.tsx
+++ packages/likec4/app/src/router.tsx
@@ -1,3 +1,4 @@
+import { Box, Loader } from '@mantine/core'
 import {
   createBrowserHistory,
   createHashHistory,
@@ -18,26 +19,15 @@ function createRouter() {
     basepath,
     trailingSlash: 'preserve',
     history: useHashHistory ? createHashHistory() : createBrowserHistory(),
-    // defaultErrorComponent
-    // defaultPendingMinMs: 600,
-    // defaultPendingMs: 300,
-    defaultPreload: false,
+    defaultStaleTime: Infinity,
     defaultNotFoundComponent: () => {
       return <NotFound />
-    }
-    // defaultPreloadDelay: 200,
-
-    // defaultPendingComponent: () => (
-    //   <Box p={'md'}>
-    //     <Loader type="dots" />
-    //   </Box>
-    // ),
-
-    // defaultStaleTime: 60_000, // 1 minute
-    // defaultPreloadStaleTime: 60_000 // 1 minute
-    // Since we're using React Query, we don't want loader calls to ever be stale
-    // This will ensure that the loader is always called when the route is preloaded or visited
-    // defaultPreloadStaleTime: 0,
+    },
+    defaultPendingComponent: () => (
+      <Box p={'md'}>
+        <Loader type="dots" />
+      </Box>
+    )
   })
 }
 
