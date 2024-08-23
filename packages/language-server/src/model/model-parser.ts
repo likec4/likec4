e8db9bd9e947360bcc39622d0a715537e4ff115e
Denis Davydkov <denis@davydkov.com>
2024-08-23 23:45:22 +0200

    fix(lsp): add MetadataAttribute to StringProperty type for Symantic Hightlights

--- packages/language-server/src/model/model-parser.ts
+++ packages/language-server/src/model/model-parser.ts
@@ -140,7 +140,7 @@ export class LikeC4ModelParser {
     const tags = this.convertTags(astNode.body)
     const stylePropsAst = astNode.body?.props.find(ast.isStyleProperties)?.props
     const style = toElementStyle(stylePropsAst)
-    const metadata = this.getMetadata(astNode.body?.props.find(ast.isMetadataProperty));
+    const metadata = this.getMetadata(astNode.body?.props.find(ast.isMetadataProperty))
     const astPath = this.getAstNodePath(astNode)
 
     let [title, description, technology] = astNode.props ?? []
@@ -186,7 +186,7 @@ export class LikeC4ModelParser {
     const tags = this.convertTags(astNode) ?? this.convertTags(astNode.body)
     const links = astNode.body?.props.filter(ast.isLinkProperty).map(p => p.value)
     const kind = astNode.kind?.ref?.name as (c4.RelationshipKind | undefined)
-    const metadata = this.getMetadata(astNode.body?.props.find(ast.isMetadataProperty));
+    const metadata = this.getMetadata(astNode.body?.props.find(ast.isMetadataProperty))
     const astPath = this.getAstNodePath(astNode)
 
     const bodyProps = mapToObj(
@@ -790,9 +790,9 @@ export class LikeC4ModelParser {
   }
 
   private getMetadata(metadataAstNode: ast.MetadataProperty | undefined): { [key: string]: string } | undefined {
-    return metadataAstNode?.props != null 
-      ? mapToObj(metadataAstNode?.props, (p) => [p.key, p.value]) 
-      : undefined;
+    return metadataAstNode?.props != null
+      ? mapToObj(metadataAstNode.props, (p) => [p.key, removeIndent(p.value)])
+      : undefined
   }
 
   private convertTags<E extends { tags?: ast.Tags }>(withTags?: E) {
