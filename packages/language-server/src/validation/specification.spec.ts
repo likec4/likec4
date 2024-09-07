f81d78d815cff3e158be864a9af30cfe3dd2971e
Denis Davydkov <denis@davydkov.com>
2024-09-07 04:32:05 +0200

    feat(dsl): warnings on repeated top blocks (instead of error)

--- packages/language-server/src/validation/specification.spec.ts
+++ packages/language-server/src/validation/specification.spec.ts
@@ -1,15 +1,15 @@
-import { afterEach, expect, test, vi } from 'vitest'
+import { afterEach, describe, it } from 'vitest'
 import { createTestServices } from '../test'
 
-const { validate, parse, validateAll, resetState } = createTestServices()
+describe('specification checks', () => {
+  const { validate, parse, validateAll, resetState } = createTestServices()
 
-afterEach(async () => {
-  await resetState()
-  // services.shared.workspace.LangiumDocuments.
-})
+  afterEach(async () => {
+    await resetState()
+  })
 
-test('specification rule checks', async () => {
-  const { errors } = await validate(`
+  it('specification rule checks', async ({ expect }) => {
+    const { warnings, errors } = await validate(`
     specification {
       element component
     }
@@ -19,140 +19,144 @@ test('specification rule checks', async () => {
     model {}
     views {}
   `)
-  expect(errors).to.have.members(['Only one specification per document is allowed'])
-})
+    expect(errors).to.be.empty
+    expect(warnings).to.have.members(['Prefer one specification per document'])
+  })
 
-test('model rule checks', async () => {
-  const { errors } = await validate(`
+  it('model rule checks', async ({ expect }) => {
+    const { errors, warnings } = await validate(`
     specification {}
     model {}
     model {}
     views {}
   `)
-  expect(errors).to.have.members(['Only one model per document is allowed'])
-})
+    expect(errors).to.be.empty
+    expect(warnings).to.have.members(['Prefer one model per document'])
+  })
 
-test('views rule checks', async () => {
-  const { errors } = await validate(`
+  it('views rule checks', async ({ expect }) => {
+    const { errors, warnings } = await validate(`
     specification {}
     views {}
     views {}
     views {}
   `)
-  expect(errors).to.have.members([
-    'Only one views block per document is allowed',
-    'Only one views block per document is allowed'
-  ])
-})
+    expect(errors).to.be.empty
+    expect(warnings).to.have.members([
+      'Prefer one views block per document',
+      'Prefer one views block per document'
+    ])
+  })
 
-test('elementKindChecks in one doc', async () => {
-  const { diagnostics } = await validate(`
+  it('elementKindChecks in one doc', async ({ expect }) => {
+    const { diagnostics } = await validate(`
     specification {
       element component
       element user
       element component
     }
   `)
-  expect(diagnostics).toHaveLength(2)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Duplicate element kind \'component\'')
-  }
-})
+    expect(diagnostics).toHaveLength(2)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Duplicate element kind \'component\'')
+    }
+  })
 
-test('elementKindChecks is not reserved word', async () => {
-  const { diagnostics } = await validate(`
+  it('elementKindChecks is not reserved word', async ({ expect }) => {
+    const { diagnostics } = await validate(`
     specification {
       element this
     }
   `)
-  expect(diagnostics).toHaveLength(1)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Reserved word: this')
-  }
-})
+    expect(diagnostics).toHaveLength(1)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Reserved word: this')
+    }
+  })
 
-test('elementKindChecks among docs', async () => {
-  await parse(`
+  it('elementKindChecks among docs', async ({ expect }) => {
+    await parse(`
     specification {
       element component
       element user
     }
   `)
-  await parse(`
+    await parse(`
     specification {
       element user
     }
   `)
 
-  const { diagnostics } = await validateAll()
-  expect(diagnostics).toHaveLength(2)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Duplicate element kind \'user\'')
-  }
-})
+    const { diagnostics } = await validateAll()
+    expect(diagnostics).toHaveLength(2)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Duplicate element kind \'user\'')
+    }
+  })
 
-test('tagChecks in one doc', async () => {
-  const { diagnostics } = await validate(`
+  it('tagChecks in one doc', async ({ expect }) => {
+    const { diagnostics } = await validate(`
     specification {
       tag tag1
       tag tag2
       tag tag1
     }
   `)
-  expect(diagnostics).toHaveLength(2)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Duplicate tag \'tag1\'')
-  }
-})
+    expect(diagnostics).toHaveLength(2)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Duplicate tag \'tag1\'')
+    }
+  })
 
-test('tagChecks among docs', async () => {
-  await parse(`
+  it('tagChecks among docs', async ({ expect }) => {
+    await parse(`
     specification {
       tag tag1
       tag tag2
     }
   `)
-  await parse(`
+    await parse(`
     specification {
       tag tag1
     }
   `)
 
-  const { diagnostics } = await validateAll()
-  expect(diagnostics).toHaveLength(2)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Duplicate tag \'tag1\'')
-  }
-})
+    const { diagnostics } = await validateAll()
+    expect(diagnostics).toHaveLength(2)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Duplicate tag \'tag1\'')
+    }
+  })
 
-test('relationshipChecks', async () => {
-  const { diagnostics } = await validate(`
+  it('relationshipChecks', async ({ expect }) => {
+    const { diagnostics } = await validate(`
     specification {
       relationship async
       relationship foo
       relationship async
     }
   `)
-  expect(diagnostics).toHaveLength(2)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Duplicate RelationshipKind \'async\'')
-  }
-})
-test('relationshipChecks is not reserved', async () => {
-  const { diagnostics } = await validate(`
+    expect(diagnostics).toHaveLength(2)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Duplicate RelationshipKind \'async\'')
+    }
+  })
+  it('relationshipChecks is not reserved', async ({ expect }) => {
+    const { diagnostics } = await validate(`
     specification {
       relationship this
     }
   `)
-  expect(diagnostics).toHaveLength(1)
-  for (const diagnostic of diagnostics) {
-    expect(diagnostic.severity, 'diagnostic severity').toBe(1)
-    expect(diagnostic.message, 'diagnostic message').toBe('Reserved word: this')
-  }
+    expect(diagnostics).toHaveLength(1)
+    for (const diagnostic of diagnostics) {
+      expect(diagnostic.severity, 'diagnostic severity').toBe(1)
+      expect(diagnostic.message, 'diagnostic message').toBe('Reserved word: this')
+    }
+  })
 })
