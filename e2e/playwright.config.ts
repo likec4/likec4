e2d46e44cb8cb0add2ba200c3ca34be348692fc8
Denis Davydkov <denis@davydkov.com>
2024-09-24 22:33:27 +0200

    fix: update e2e workflow to use latest create-pull-request action and adjust screenshot paths

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,7 +4,7 @@ import { isCI } from 'std-env'
 export default defineConfig({
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'tests',
-  snapshotPathTemplate: '{testDir}/__screenshots__{/projectName}/{testFilePath}/{arg}{ext}',
+  snapshotPathTemplate: '{testDir}/__screenshots__/{projectName}-{platform}/{arg}{ext}',
 
   // Run all tests in parallel.
   fullyParallel: true,
@@ -13,10 +13,11 @@ export default defineConfig({
   forbidOnly: isCI,
 
   // Retry on CI only.
-  retries: isCI ? 2 : 0,
+  retries: isCI ? 1 : 0,
 
   // Opt out of parallel tests on CI.
-  workers: isCI ? 1 : '80%',
+  // workers: isCI ? /1 : '80%',
+  workers: '100%',
 
   // Reporter to use
   reporter: 'html',
@@ -26,8 +27,9 @@ export default defineConfig({
     // Base URL to use in actions like `await page.goto('/')`.
     // baseURL: 'http://127.0.0.1:5173',
 
-    // Collect trace when retrying the failed test.
-    trace: 'retain-on-failure'
+    deviceScaleFactor: 2,
+    trace: 'on-first-retry',
+    video: 'on-first-retry'
   },
 
   expect: {
