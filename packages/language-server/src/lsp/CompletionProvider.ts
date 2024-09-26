f349abaed88befe2e310ec1f531f58c855e7a3b3
Denis Davydkov <denis@davydkov.com>
2024-09-26 14:14:37 +0200

    feat(vscode): code snippets

--- packages/language-server/src/lsp/CompletionProvider.ts
+++ packages/language-server/src/lsp/CompletionProvider.ts
@@ -1,7 +1,51 @@
-import { type CompletionProviderOptions, DefaultCompletionProvider } from 'langium/lsp'
+import { type GrammarAST, type MaybePromise } from 'langium'
+import {
+  type CompletionAcceptor,
+  type CompletionContext,
+  type CompletionProviderOptions,
+  DefaultCompletionProvider
+} from 'langium/lsp'
+import { CompletionItemKind, InsertTextFormat } from 'vscode-languageserver-types'
 
 export class LikeC4CompletionProvider extends DefaultCompletionProvider {
   override readonly completionOptions = {
     triggerCharacters: ['.']
   } satisfies CompletionProviderOptions
+
+  protected override completionForKeyword(
+    context: CompletionContext,
+    keyword: GrammarAST.Keyword,
+    acceptor: CompletionAcceptor
+  ): MaybePromise<void> {
+    if (['views', 'specification', 'model'].includes(keyword.value)) {
+      return acceptor(context, {
+        label: keyword.value,
+        detail: `Insert ${keyword} block`,
+        kind: CompletionItemKind.Module,
+        insertTextFormat: InsertTextFormat.Snippet,
+        insertText: `${keyword} {\n\t$0\n}`
+      })
+    }
+    if (keyword.value === 'dynamic') {
+      return acceptor(context, {
+        label: keyword.value,
+        detail: `Insert dynamic view`,
+        kind: CompletionItemKind.Class,
+        insertTextFormat: InsertTextFormat.Snippet,
+        insertText: [
+          'dynamic view ${1:view_${TM_FILENAME_BASE}_${CURRENT_SECOND}} {',
+          '\ttitle \'${2:Untitled}\'',
+          '\t',
+          '\t$0',
+          '}'
+        ].join('\n')
+      })
+    }
+    acceptor(context, {
+      label: keyword.value,
+      kind: this.getKeywordCompletionItemKind(keyword),
+      detail: 'Keyword',
+      sortText: '1'
+    })
+  }
 }
