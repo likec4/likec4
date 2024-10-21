ed70413c0960968d9e9369bd712e41cb269e9e56
Denis Davydkov <denis@davydkov.com>
2024-10-21 04:04:04 +0200

    add spec for object equality

--- packages/core/src/utils/commonHead.spec.ts
+++ packages/core/src/utils/commonHead.spec.ts
@@ -9,29 +9,37 @@ test('commonHead of two empty arrays', () => {
 })
 
 test('commonHead of one empty array and one non-empty array', () => {
-  const sources: number[] = [1, 2, 3]
+  const sources = [1, 2, 3]
   const targets: number[] = []
   const result = commonHead(sources, targets)
   expect(result).toEqual([])
 })
 
 test('commonHead of two arrays with no common elements', () => {
-  const sources: number[] = [1, 2, 3]
-  const targets: number[] = [4, 5, 6]
+  const sources = [1, 2, 3]
+  const targets = [4, 5, 6]
   const result = commonHead(sources, targets)
   expect(result).toEqual([])
 })
 
 test('commonHead of two arrays with some common elements at the start', () => {
-  const sources: number[] = [1, 2, 3, 4]
-  const targets: number[] = [1, 2, 5, 6]
+  const sources = [1, 2, 3, 4]
+  const targets = [1, 2, 5, 6]
   const result = commonHead(sources, targets)
   expect(result).toEqual([1, 2])
 })
 
+test('commonHead of two arrays with some common object elements at the start', () => {
+  const A = { a: 1 }, B = { b: 2 }
+  const sources = [A, B, { c: 3 }]
+  const targets = [A, B, { d: 4 }]
+  const result = commonHead<unknown>(sources, targets)
+  expect(result).toEqual([A, B])
+})
+
 test('commonHead of two arrays with all elements in common', () => {
-  const sources: number[] = [1, 2, 3]
-  const targets: number[] = [1, 2, 3]
+  const sources = [1, 2, 3]
+  const targets = [1, 2, 3]
   const result = commonHead(sources, targets)
   expect(result).toEqual([1, 2, 3])
 })
