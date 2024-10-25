cf59edf1343ae765750b3eaa3c85eef3a23a0159
Hubert Miś <hubert.mis@nordicsemi.no>
2024-10-26 01:00:38 +0200

    feat: global style groups (#1143)
    Allow defining style groups with a set of style rules that can be
    applied to multiple views.
    
    This is the 3rd PR of a series aiming to replace #1058

--- packages/language-server/src/model/model-parser.ts
+++ packages/language-server/src/model/model-parser.ts
@@ -30,7 +30,7 @@ import {
   ViewOps
 } from '../ast'
 import { elementRef, getFqnElementRef } from '../elementRef'
-import { type NotationProperty } from '../generated/ast'
+import { isGlobalStyle, isGlobalStyleGroup, type NotationProperty } from '../generated/ast'
 import { logError, logger, logWarnError } from '../logger'
 import type { LikeC4Services } from '../module'
 import { stringHash } from '../utils'
@@ -291,7 +291,7 @@ export class LikeC4ModelParser {
     const styles = globals.flatMap(r => r.styles.filter(isValid))
     for (const style of styles) {
       try {
-        const globalStyleId = style.name as c4.GlobalStyleID
+        const globalStyleId = style.id.name as c4.GlobalStyleID
         if (!isTruthy(globalStyleId)) {
           continue
         }
@@ -299,13 +299,28 @@ export class LikeC4ModelParser {
           logger.warn(`Global style named "${globalStyleId}" is already defined`)
           continue
         }
-        c4Globals.styles[globalStyleId] = [
-          this.parseGlobalStyle(style, isValid)
-        ]
+
+        const styles = this.parseGlobalStyleOrGroup(style, isValid)
+        if (styles.length > 0) {
+          c4Globals.styles[globalStyleId] = styles as c4.NonEmptyArray<c4.ViewRuleStyle>
+        }
       } catch (e) {
         logWarnError(e)
       }
     }
+  }
+
+  private parseGlobalStyleOrGroup(
+    astRule: ast.GlobalStyle | ast.GlobalStyleGroup,
+    isValid: IsValidFn
+  ): c4.ViewRuleStyle[] {
+    if (ast.isGlobalStyle(astRule)) {
+      return [this.parseGlobalStyle(astRule, isValid)]
+    }
+    if (ast.isGlobalStyleGroup(astRule)) {
+      return astRule.styles.map(s => this.parseViewRuleStyle(s, isValid))
+    }
+    nonexhaustive(astRule)
   }
 
   private parseGlobalStyle(astRule: ast.GlobalStyle, isValid: IsValidFn): c4.ViewRuleStyle {
@@ -675,7 +690,7 @@ export class LikeC4ModelParser {
     }
   }
 
-  private parseViewRuleGlobalStyle(astRule: ast.ViewRuleGlobalStyle, isValid: IsValidFn): c4.ViewRuleGlobalStyle {
+  private parseViewRuleGlobalStyle(astRule: ast.ViewRuleGlobalStyle, _isValid: IsValidFn): c4.ViewRuleGlobalStyle {
     return {
       styleId: astRule.style.$refText as c4.GlobalStyleID
     }
