d2046dafba37d704175de660f453d8213f6e7427
Hubert Miś <hubert.mis@nordicsemi.no>
2024-11-09 00:43:30 +0100

    feat: traverse symlinks in the workspace (#1213)
    A likeC4 workspace can contain symbolic links to documents shared
    between multiple workspaces. The WorspaceManager is enhanced to traverse
    through symbolic links to correctly include documents linked by symbolic
    links.

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -1,5 +1,6 @@
+import { URI, UriUtils } from 'langium'
 import { startLanguageServer as startLanguim } from 'langium/lsp'
-import { NodeFileSystem } from 'langium/node'
+import { NodeFileSystemProvider } from 'langium/node'
 import { createConnection, ProposedFeatures } from 'vscode-languageserver/node'
 import { createLanguageServices } from './module'
 
@@ -8,12 +9,16 @@ export type * from './model'
 export type * from './module'
 export { createCustomLanguageServices, createLanguageServices, LikeC4Module } from './module'
 
+import type { FileSystemNode } from 'langium'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+
 export function startLanguageServer() {
   /* browser specific setup code */
   const connection = createConnection(ProposedFeatures.all)
 
   // Inject the shared services and language-specific services
-  const services = createLanguageServices({ connection, ...NodeFileSystem })
+  const services = createLanguageServices({ connection, ...LikeC4FileSystem })
 
   // Start the language server with the shared services
   startLanguim(services.shared)
@@ -21,5 +26,32 @@ export function startLanguageServer() {
   return {
     ...services,
     connection
+  }
+}
+
+export const LikeC4FileSystem = {
+  fileSystemProvider: () => new SymLinkTraversingFileSystemProvider()
+}
+
+class SymLinkTraversingFileSystemProvider extends NodeFileSystemProvider {
+  override async readDirectory(folderPath: URI): Promise<FileSystemNode[]> {
+    const dirents = await fs.promises.readdir(folderPath.fsPath, { withFileTypes: true })
+    return dirents.map(dirent => (this.followUri(UriUtils.joinPath(folderPath, dirent.name))))
+  }
+
+  followUri(uri: URI): FileSystemNode {
+    const directoryPath = path.dirname(uri.fsPath)
+    const stat = fs.lstatSync(uri.fsPath)
+    if (stat.isSymbolicLink()) {
+      const resolved_link = fs.readlinkSync(uri.fsPath)
+      const linked_path = path.resolve(directoryPath, resolved_link)
+      return this.followUri(URI.file(linked_path))
+    } else {
+      return {
+        isFile: stat.isFile(),
+        isDirectory: stat.isDirectory(),
+        uri
+      }
+    }
   }
 }
