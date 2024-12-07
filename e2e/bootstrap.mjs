6feb2ace03a2c86b11d066dd6540d31ede4b134b
Denis Davydkov <denis@davydkov.com>
2024-12-07 02:16:10 +0100

    fix: update e2e tests configuration, enhance snapshot generation, and adjust retries and timeout settings

--- e2e/bootstrap.mjs
+++ e2e/bootstrap.mjs
@@ -13,26 +13,56 @@ const likec4 = await LikeC4.fromWorkspace('src/likec4', {
   logger: 'default',
   throwIfInvalid: true
 })
-const views = await likec4.viewsService.computedViews()
+const computedModel = likec4.computedModel()
+const computedViews = [...computedModel.views()].map(v => v.id)
 
-const tests = views.map((view) => {
-  return `
+const layoutedModel = await likec4.layoutedModel()
+const layoutedViews = [...layoutedModel.views()].map(v => v.id)
+
+if (computedViews.length !== layoutedViews.length) {
+  throw new Error('Computed views and layouted views are not equal')
+}
+
+for (const view of layoutedModel.views()) {
+  const content = `
+import { test, expect } from "@playwright/test";
+
 test('${view.id} - compare snapshots', async ({ page }) => {
+  console.log('Compare - ${view.id}, go to /export/${encodeURIComponent(view.id)}?padding=20')
+  await page.setViewportSize({ width: ${view.$view.bounds.width + 40}, height: ${view.$view.bounds.height + 40} });
   await page.goto('/export/${encodeURIComponent(view.id)}?padding=20');
   const diagramElement = page.getByRole('presentation')
+  console.log('waitFor - ${view.id}')
   await diagramElement.waitFor()
   await expect(diagramElement).toHaveScreenshot('${view.id}.png', {
     omitBackground: true
   });
 });
-  `
-})
-
-const content = `
-import { test, expect } from "@playwright/test";
-
-test.describe.configure({ mode: 'parallel' });
-
-${tests.join('')}
 `
-await fs.writeFile('tests/snapshot.spec.ts', content, { encoding: 'utf-8' })
+  await fs.writeFile(`tests/${view.id}.spec.ts`, content, { encoding: 'utf-8' })
+  echo(`Generated tests/${view.id}.spec.ts`)
+}
+
+// const tests = views.map((view) => {
+//   return `
+// import { test, expect } from "@playwright/test";
+
+// test('${view.id} - compare snapshots', async ({ page }) => {
+//   await page.goto('/export/${encodeURIComponent(view.id)}?padding=20');
+//   const diagramElement = page.getByRole('presentation')
+//   await diagramElement.waitFor()
+//   await expect(diagramElement).toHaveScreenshot('${view.id}.png', {
+//     omitBackground: true
+//   });
+// });
+//   `
+// })
+
+// // const content = `
+// // import { test, expect } from "@playwright/test";
+
+// // test.describe.configure({ mode: 'parallel' });
+
+// // ${tests.join('')}
+// // `
+// await fs.writeFile('tests/snapshot.spec.ts', content, { encoding: 'utf-8' })
--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -13,31 +13,32 @@ export default defineConfig({
   forbidOnly: isCI,
 
   // Retry on CI only.
-  retries: isCI ? 2 : 0,
-  timeout: isCI ? 10 * 1000 : undefined,
+  retries: isCI ? 1 : 0,
+  timeout: 10 * 1000,
 
   // Opt out of parallel tests on CI.
   // workers: isCI ? /1 : '80%',
-  workers: isCI ? '80%' : '100%',
+  workers: isCI ? '75%' : '100%',
 
   // Reporter to use
-  reporter: 'html',
+  reporter: isCI
+    ? [
+      ['github'],
+      ['list'],
+      ['html']
+    ]
+    : 'html',
 
   updateSnapshots: 'missing',
   use: {
-    viewport: { width: 1920, height: 1080 },
-    // Base URL to use in actions like `await page.goto('/')`.
-    // baseURL: 'http://127.0.0.1:5173',
-    ignoreHTTPSErrors: true,
-    deviceScaleFactor: 2,
-    trace: 'on-first-retry',
-    video: 'off'
+    browserName: 'chromium',
+    trace: 'retain-on-first-failure'
   },
 
   expect: {
     toHaveScreenshot: {
       scale: 'device',
-      maxDiffPixelRatio: 0.0005
+      maxDiffPixelRatio: 0.05
     }
   },
 
