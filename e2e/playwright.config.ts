d2f46e490305f804bfcd65182028ed5b0ca385b9
Denis Davydkov <denis@davydkov.com>
2024-12-07 01:35:35 +0100

    fix: increase retries and timeout for e2e tests, adjust worker percentage, and enhance configuration

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -13,20 +13,22 @@ export default defineConfig({
   forbidOnly: isCI,
 
   // Retry on CI only.
-  retries: isCI ? 1 : 0,
+  retries: isCI ? 2 : 0,
+  timeout: isCI ? 10 * 1000 : undefined,
 
   // Opt out of parallel tests on CI.
   // workers: isCI ? /1 : '80%',
-  workers: isCI ? '75%' : '100%',
+  workers: isCI ? '80%' : '100%',
 
   // Reporter to use
   reporter: 'html',
 
   updateSnapshots: 'missing',
   use: {
+    viewport: { width: 1920, height: 1080 },
     // Base URL to use in actions like `await page.goto('/')`.
     // baseURL: 'http://127.0.0.1:5173',
-
+    ignoreHTTPSErrors: true,
     deviceScaleFactor: 2,
     trace: 'on-first-retry',
     video: 'off'
@@ -49,7 +51,9 @@ export default defineConfig({
   // Run your local dev server before starting the tests.
   webServer: {
     command: 'yarn likec4 start',
-    port: 5173
+    port: 5173,
+    stdout: 'pipe',
+    timeout: 10 * 1000
     // url: 'http://127.0.0.1:5173',
     // reuseExistingServer: !process.env.CI
   }
