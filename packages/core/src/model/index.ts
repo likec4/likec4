21b12cabd3f9f08e1c5f741f7b1084218dcc40b1
Denis Davydkov <denis@davydkov.com>
2024-12-17 06:01:23 +0100

    fix: update imports and type annotations in LikeC4Model integration

--- packages/core/src/model/index.ts
+++ packages/core/src/model/index.ts
@@ -1,5 +1,11 @@
 export { LikeC4DeploymentModel } from './DeploymentModel'
 export { LikeC4Model } from './LikeC4Model'
-export type * from './LikeC4Model'
-export type { Aux as LikeC4ModelAux } from './types'
 export { LikeC4ViewModel } from './view/LikeC4ViewModel'
+
+export type * from './connection/index'
+export type * from './DeploymentElementModel'
+export type * from './DeploymentModel'
+export type * from './ElementModel'
+export type * from './ElementModel'
+export type * from './LikeC4Model'
+export type * from './types'
--- packages/likec4/app/react/components/createLikeC4Model.ts
+++ packages/likec4/app/react/components/createLikeC4Model.ts
@@ -1,11 +1,11 @@
-import { type LayoutedLikeC4Model, LikeC4Model } from '@likec4/core'
+import { LikeC4Model } from '@likec4/core/model'
 
 /**
  * We need this function to bundle LikeC4Model with `likec4/react'
  * Used by vite plugin to generate `virtual:likec4/model`
  */
-export function createLikeC4Model(model: LayoutedLikeC4Model): LikeC4Model.Layouted {
-  return LikeC4Model.create(model)
+export function createLikeC4Model(model: any): LikeC4Model {
+  return LikeC4Model.create(model) as LikeC4Model
 }
 
-export { LikeC4Model } from '@likec4/core'
+export { LikeC4Model } from '@likec4/core/model'
