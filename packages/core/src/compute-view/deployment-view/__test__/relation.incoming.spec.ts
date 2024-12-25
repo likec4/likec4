a82e2250fd06f5d0ee2ded07c2ecc2888f6e3ebf
Pykhtin Pavel <pavel.pykhtin@gmail.com>
2024-12-25 12:13:43 +0300

    Added tests on relation predicates

--- packages/core/src/compute-view/deployment-view/__test__/relation.incoming.spec.ts
+++ packages/core/src/compute-view/deployment-view/__test__/relation.incoming.spec.ts
@@ -6,31 +6,147 @@ function expectComputed(...rules: DeploymentViewRuleBuilderOp<Types>[]) {
   return expect(computeNodesAndEdges(...rules))
 }
 
-describe('DirectRelationPredicate', () => {
-  it('should include direct relation if it matches condition', () => {
+describe('IncommingRelationPredicate', () => {
+  it('should include relation incoming to deployment node if it matches condition', () => {
     expectComputed(
-      $include('customer -> prod.eu', {where: 'tag is #old'})
+      $include('customer'),
+      $include('-> prod.eu.zone1.ui', { where: 'tag is #old' })
     ).toMatchInlineSnapshot(`
       {
         "Nodes": [
           "customer",
-          "prod.eu",
+          "prod.eu.zone1.ui",
         ],
         "edges": [
-          "customer:prod.eu",
+          "customer:prod.eu.zone1.ui",
         ],
       }
     `)
   })
 
-  it('should not include direct relation if it does not match condition', () => {
+  it('should not include relation incoming to deployment node if it does not match condition', () => {
     expectComputed(
-      $include('customer -> prod.eu', {where: 'tag is #next'})
+      $include('customer'),
+      $include('-> prod.eu.zone1.ui', { where: 'tag is #next' })
     ).toMatchInlineSnapshot(`
       {
-        "Nodes": [],
+        "Nodes": [
+          "customer",
+        ],
         "edges": [],
       }
     `)
+  })
+
+  it('should exclude relation incoming to model element if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        incoming: {
+          ref: {
+            model: 'cloud.backend'
+          }
+        }
+      }, { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media',
+      ],
+      edges: [
+        "prod.eu.zone1.api:prod.eu.auth",
+        "prod.eu.zone1.api:prod.eu.media",
+        "prod.eu.zone1.api:prod.eu.db",
+        "prod.eu.zone1.ui:prod.eu.auth",
+        "prod.eu.zone1.ui:prod.eu.media",
+      ]
+    })
+  })
+
+  it('should not exclude relation incoming to model element if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        incoming: {
+          ref: {
+            model: 'cloud.backend'
+          }
+        }
+      }, { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        "prod.eu.zone1",
+        "prod.eu.zone1.ui",
+        "prod.eu.zone1.api",
+        "prod.eu.auth",
+        "prod.eu.media",
+        "prod.eu.db",
+      ],
+      edges: [
+        "prod.eu.zone1.ui:prod.eu.zone1.api",
+        "prod.eu.zone1.api:prod.eu.auth",
+        "prod.eu.zone1.api:prod.eu.media",
+        "prod.eu.zone1.api:prod.eu.db",
+        "prod.eu.zone1.ui:prod.eu.auth",
+        "prod.eu.zone1.ui:prod.eu.media",]
+    })
+  })  
+
+  it('should exclude relation incoming to deployment node if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude("-> prod.eu.zone1.*", { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media',
+      ],
+      edges: [
+        "prod.eu.zone1.api:prod.eu.auth",
+        "prod.eu.zone1.api:prod.eu.media",
+        "prod.eu.zone1.api:prod.eu.db",
+        "prod.eu.zone1.ui:prod.eu.auth",
+        "prod.eu.zone1.ui:prod.eu.media",
+      ]
+    })
+  })
+
+  it('should not exclude relation incoming to deployment node if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude("-> prod.eu.zone1.*", { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        "prod.eu.zone1",
+        "prod.eu.zone1.ui",
+        "prod.eu.zone1.api",
+        "prod.eu.auth",
+        "prod.eu.media",
+        "prod.eu.db",
+      ],
+      edges: [
+        "prod.eu.zone1.ui:prod.eu.zone1.api",
+        "prod.eu.zone1.api:prod.eu.auth",
+        "prod.eu.zone1.api:prod.eu.media",
+        "prod.eu.zone1.api:prod.eu.db",
+        "prod.eu.zone1.ui:prod.eu.auth",
+        "prod.eu.zone1.ui:prod.eu.media",]
+    })
   })
 })
---
+++ packages/core/src/compute-view/deployment-view/__test__/relation.inout.spec.ts
@@ -0,0 +1,299 @@
+import { describe, expect, it } from 'vitest'
+import type { DeploymentViewRuleBuilderOp } from '../../../builder/Builder.view'
+import { $exclude, $include, computeNodesAndEdges, type Types } from './fixture'
+
+function expectComputed(...rules: DeploymentViewRuleBuilderOp<Types>[]) {
+  return expect(computeNodesAndEdges(...rules))
+}
+
+describe('InoutRelationPredicate', () => {
+  it('should include relation incoming to deployment node if it matches condition', () => {
+    expectComputed(
+      $include('customer'),
+      $include('-> prod.eu.zone1.ui ->', { where: 'tag is #old' })
+    ).toMatchInlineSnapshot(`
+      {
+        "Nodes": [
+          "customer",
+          "prod.eu.zone1.ui",
+        ],
+        "edges": [
+          "customer:prod.eu.zone1.ui",
+        ],
+      }
+    `)
+  })
+
+  it('should include relation outgoing from deployment node if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.zone1.api'),
+      $include('-> prod.eu.zone1.ui ->', { where: 'tag is #old' })
+    ).toMatchInlineSnapshot(`
+      {
+        "Nodes": [
+          "prod.eu.zone1.ui",
+          "prod.eu.zone1.api",
+        ],
+        "edges": [
+          "prod.eu.zone1.ui:prod.eu.zone1.api",
+        ],
+      }
+    `)
+  })
+
+  it('should not include relation incoming to deployment node if it does not match condition', () => {
+    expectComputed(
+      $include('customer'),
+      $include('-> prod.eu.zone1.ui ->', { where: 'tag is #next' })
+    ).toMatchInlineSnapshot(`
+      {
+        "Nodes": [
+          "customer",
+        ],
+        "edges": [],
+      }
+    `)
+  })
+
+  it('should not include relation outgoing from deployment node if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.zone1.api'),
+      $include('-> prod.eu.zone1.ui ->', { where: 'tag is #next' })
+    ).toMatchInlineSnapshot(`
+      {
+        "Nodes": [
+          "prod.eu.zone1.api",
+        ],
+        "edges": [],
+      }
+    `)
+  })
+
+  it('should exclude relation incoming to model element if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        inout: {
+          ref: {
+            model: 'cloud.backend'
+          }
+        }
+      }, { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media'
+      ],
+      edges: [
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should exclude relation outgoing from model element if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        outgoing: {
+          ref: {
+            model: 'cloud.frontend'
+          }
+        }
+      }, { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media'
+      ],
+      edges: [
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should not exclude relation incoming to model element if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        inout: {
+          ref: {
+            model: 'cloud.backend'
+          }
+        }
+      }, { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.ui',
+        'prod.eu.zone1.api',
+        'prod.eu.auth',
+        'prod.eu.media',
+        'prod.eu.db'
+      ],
+      edges: [
+        'prod.eu.zone1.ui:prod.eu.zone1.api',
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should not exclude relation outgoing from model element if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude({
+        outgoing: {
+          ref: {
+            model: 'cloud.frontend'
+          }
+        }
+      }, { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.ui',
+        'prod.eu.zone1.api',
+        'prod.eu.auth',
+        'prod.eu.media',
+        'prod.eu.db'
+      ],
+      edges: [
+        'prod.eu.zone1.ui:prod.eu.zone1.api',
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should exclude relation incoming to deployment node if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude('-> prod.eu.zone1.* ->', { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media'
+      ],
+      edges: [
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should exclude relation outgoing from deployment node if it matches condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude('-> prod.eu.zone1.* ->', { where: 'tag is #old' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.api',
+        'prod.eu.zone1.ui',
+        'prod.eu.db',
+        'prod.eu.auth',
+        'prod.eu.media'
+      ],
+      edges: [
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should not exclude relation incoming to deployment node if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude('-> prod.eu.zone1.* ->', { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.ui',
+        'prod.eu.zone1.api',
+        'prod.eu.auth',
+        'prod.eu.media',
+        'prod.eu.db'
+      ],
+      edges: [
+        'prod.eu.zone1.ui:prod.eu.zone1.api',
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+
+  it('should not exclude relation outgoing from deployment node if it does not match condition', () => {
+    expectComputed(
+      $include('prod.eu.*'),
+      $include('prod.eu.zone1.**'),
+      $exclude('prod.eu.zone2'),
+      $exclude('-> prod.eu.zone1.* ->', { where: 'tag is #next' })
+    ).toEqual({
+      Nodes: [
+        'prod.eu.zone1',
+        'prod.eu.zone1.ui',
+        'prod.eu.zone1.api',
+        'prod.eu.auth',
+        'prod.eu.media',
+        'prod.eu.db'
+      ],
+      edges: [
+        'prod.eu.zone1.ui:prod.eu.zone1.api',
+        'prod.eu.zone1.api:prod.eu.auth',
+        'prod.eu.zone1.api:prod.eu.media',
+        'prod.eu.zone1.api:prod.eu.db',
+        'prod.eu.zone1.ui:prod.eu.auth',
+        'prod.eu.zone1.ui:prod.eu.media'
+      ]
+    })
+  })
+})
