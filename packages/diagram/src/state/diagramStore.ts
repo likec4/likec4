d2fd71cfe396966d7bbe84c53a583950ec4a1817
Pykhtin Pavel <pavel.pykhtin@gmail.com>
2024-12-27 15:48:03 +0300

    Fix 'reset connection points' on self-referencing relations

--- packages/diagram/src/state/diagramStore.ts
+++ packages/diagram/src/state/diagramStore.ts
@@ -42,7 +42,7 @@ import type {
   LikeC4DiagramEventHandlers,
   WhereOperator
 } from '../LikeC4Diagram.props'
-import { type Vector, vector } from '../utils/vector'
+import { type Vector, vector, VectorImpl } from '../utils/vector'
 import { MinZoom } from '../xyflow/const'
 import type { XYFlowEdge, XYFlowInstance, XYFlowNode } from '../xyflow/types'
 import { bezierControlPoints, isInside, isSamePoint, toDomPrecision } from '../xyflow/utils'
@@ -1065,15 +1065,28 @@ export function createDiagramStore(props: DiagramInitialState) {
               const sourceCenter = getNodeCenter(source, xynodes)
               const targetCenter = getNodeCenter(target, xynodes)
 
-              if (sourceCenter && targetCenter) {
-                const sourceToTargetVector = targetCenter.sub(sourceCenter)
-                const sourceBorderPoint = getBorderPointOnVector(source, sourceCenter, sourceToTargetVector)
-                const targetBorderPoint = getBorderPointOnVector(target, targetCenter, sourceToTargetVector.mul(-1))
-
-                return [sourceBorderPoint.add(targetBorderPoint.sub(sourceBorderPoint).mul(0.3))]
+              if (!sourceCenter || !targetCenter) {
+                return []
               }
 
-              return []
+              // Edge is a loop
+              if(source == target) {
+                const loopSize = 80                
+                const centerOfTopBoundary = new VectorImpl(0, source.height || 0)
+                  .mul(-0.5)
+                  .add(sourceCenter)
+
+                return [
+                  centerOfTopBoundary.add(new VectorImpl(-loopSize / 2.5, -loopSize)),
+                  centerOfTopBoundary.add(new VectorImpl(loopSize / 2.5, -loopSize))
+                ]
+              }
+
+              const sourceToTargetVector = targetCenter.sub(sourceCenter)
+              const sourceBorderPoint = getBorderPointOnVector(source, sourceCenter, sourceToTargetVector)
+              const targetBorderPoint = getBorderPointOnVector(target, targetCenter, sourceToTargetVector.mul(-1))
+
+              return [sourceBorderPoint.add(targetBorderPoint.sub(sourceBorderPoint).mul(0.3))]
             }
 
             function getBorderPointOnVector(node: XYFlowNode, nodeCenter: Vector, v: Vector) {
