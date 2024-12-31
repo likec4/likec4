2489191c67117675cc7486c130bfc4c0a3247324
Denis Davydkov <denis@davydkov.com>
2024-12-31 09:00:32 +0100

    handle undefined arguments more gracefully

--- packages/core/src/utils/iterable/reduce.ts
+++ packages/core/src/utils/iterable/reduce.ts
@@ -1,14 +1,14 @@
-import { isFunction } from 'remeda'
+import { isDefined, isFunction } from 'remeda'
 import { invariant } from '../../errors'
 
 export function ireduce<T, R>(reducer: (acc: R, item: T) => R, initialValue: R): (iterable: Iterable<T>) => R
 export function ireduce<T, R>(iterable: Iterable<T>, reducer: (acc: R, item: T) => R, initialValue: R): R
 export function ireduce<T, R>(
   arg1: unknown,
-  arg2?: unknown,
+  arg2: unknown,
   arg3?: unknown,
 ): R | ((iterable: Iterable<T>) => R) {
-  const reducer = (arg2 ?? arg1) as (acc: R, item: T) => R
+  const reducer = (isDefined(arg3) ? arg2 : arg1) as (acc: R, item: T) => R
   const initialValue = arg3 ?? arg2
   invariant(isFunction(reducer))
 
@@ -20,8 +20,5 @@ export function ireduce<T, R>(
     return acc
   }
 
-  if (!arg3) {
-    return _reduce
-  }
-  return _reduce(arg1 as Iterable<T>)
+  return isDefined(arg3) ? _reduce(arg1 as Iterable<T>) : _reduce
 }
