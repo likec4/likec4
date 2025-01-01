eae888fe0bcd8bf011013395fa53510da3b0475f
Denis Davydkov <denis@davydkov.com>
2025-01-01 22:10:04 +0100

    fix: add error handling and logging in documentation provider

--- packages/language-server/src/documentation/documentation-provider.ts
+++ packages/language-server/src/documentation/documentation-provider.ts
@@ -1,5 +1,6 @@
 import { type AstNode, type DocumentationProvider, AstUtils } from 'langium'
 import { ast } from '../ast'
+import { logWarnError } from '../logger'
 import type { LikeC4ModelLocator, LikeC4ModelParser } from '../model'
 import type { LikeC4Services } from '../module'
 
@@ -12,35 +13,39 @@ export class LikeC4DocumentationProvider implements DocumentationProvider {
     this.locator = services.likec4.ModelLocator
   }
   getDocumentation(node: AstNode): string | undefined {
-    if (ast.isDeploymentNode(node)) {
-      const doc = AstUtils.getDocument(node)
-      const el = this.parser.forDocument(doc).parseDeploymentNode(node)
-      const lines = [el.id as string]
-      if (el.title !== node.name) {
-        lines.push(' ', `**${el.title}**`)
+    try {
+      if (ast.isDeploymentNode(node)) {
+        const doc = AstUtils.getDocument(node)
+        const el = this.parser.forDocument(doc).parseDeploymentNode(node)
+        const lines = [el.id as string]
+        if (el.title !== node.name) {
+          lines.push(' ', `**${el.title}**`)
+        }
+        return lines.join('  \n')
       }
-      return lines.join('  \n')
-    }
 
-    if (ast.isDeployedInstance(node)) {
-      const doc = AstUtils.getDocument(node)
-      const instance = this.parser.forDocument(doc).parseDeployedInstance(node)
-      const el = this.locator.getParsedElement(instance.element)
-      const lines = [instance.id, `_instance of_ ${instance.element}`]
-      if (el) {
-        lines.push(' ', `**${el.title}**`)
+      if (ast.isDeployedInstance(node)) {
+        const doc = AstUtils.getDocument(node)
+        const instance = this.parser.forDocument(doc).parseDeployedInstance(node)
+        const el = this.locator.getParsedElement(instance.element)
+        const lines = [instance.id, `_instance of_ ${instance.element}`]
+        if (el) {
+          lines.push(' ', `**${el.title}**`)
+        }
+        return lines.join('  \n')
       }
-      return lines.join('  \n')
-    }
 
-    if (ast.isElement(node)) {
-      const doc = AstUtils.getDocument(node)
-      const el = this.parser.forDocument(doc).parseElement(node)
-      if (!el) {
-        return
+      if (ast.isElement(node)) {
+        const doc = AstUtils.getDocument(node)
+        const el = this.parser.forDocument(doc).parseElement(node)
+        if (!el) {
+          return
+        }
+        const lines = [el.id, ' ', `**${el.title}**`]
+        return lines.join('  \n')
       }
-      const lines = [el.id, ' ', `**${el.title}**`]
-      return lines.join('  \n')
+    } catch (e) {
+      logWarnError(e)
     }
     return
   }
