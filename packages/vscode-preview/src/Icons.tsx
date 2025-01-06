7f7760768cd1983c0016d5290abd49a4fd7bf45a
Denis Davydkov <denis@davydkov.com>
2025-01-06 22:54:23 +0100

    fix: disable dynamic import in vscode extension
    Fixes #1415

--- packages/vscode-preview/src/Icons.tsx
+++ packages/vscode-preview/src/Icons.tsx
@@ -1,16 +1,9 @@
 import type { ElementIconRenderer } from '@likec4/diagram'
-import { IconLoader } from '@tabler/icons-react'
-import { lazy, Suspense } from 'react'
-
-const Icons = lazy(() => import('@likec4/icons/all'))
+import Icons from '@likec4/icons/all'
 
 export const IconRenderer: ElementIconRenderer = ({ node }) => {
   if (!node.icon || node.icon === 'none') {
     return null
   }
-  return (
-    <Suspense fallback={<IconLoader />}>
-      <Icons name={(node.icon ?? '') as any} />
-    </Suspense>
-  )
+  return <Icons name={(node.icon ?? '') as any} />
 }
