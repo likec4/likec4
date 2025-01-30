2a0b8aac6e2574da3c19161e6e61c76582d65f81
Denis Davydkov <denis@davydkov.com>
2025-01-30 05:58:12 +0100

    chore(e2e): add model generation step and clean up unused code

--- e2e/bootstrap.mjs
+++ e2e/bootstrap.mjs
@@ -7,6 +7,9 @@ import { LikeC4 } from 'likec4'
 echo(chalk.greenBright('\n-------- Generate React component --------'))
 await $({ stdio: 'inherit' })`likec4 codegen react ./src`
 
+echo(chalk.greenBright('\n-------- Generate Model --------'))
+await $({ stdio: 'inherit' })`likec4 codegen model ./src`
+
 echo(chalk.greenBright('\n-------- Generate Tests --------'))
 
 const likec4 = await LikeC4.fromWorkspace('src/likec4', {
@@ -40,29 +43,3 @@ test('${view.id} - compare snapshots', async ({ page }) => {
   await fs.writeFile(`tests/${view.id}-gen.spec.ts`, content, { encoding: 'utf-8' })
   echo(`Generated tests/${view.id}-gen.spec.ts`)
 }
-
-$`npx likec4 codegen model src`
-
-// const tests = views.map((view) => {
-//   return `
-// import { test, expect } from "@playwright/test";
-
-// test('${view.id} - compare snapshots', async ({ page }) => {
-//   await page.goto('/export/${encodeURIComponent(view.id)}?padding=20');
-//   const diagramElement = page.getByRole('presentation')
-//   await diagramElement.waitFor()
-//   await expect(diagramElement).toHaveScreenshot('${view.id}.png', {
-//     omitBackground: true
-//   });
-// });
-//   `
-// })
-
-// // const content = `
-// // import { test, expect } from "@playwright/test";
-
-// // test.describe.configure({ mode: 'parallel' });
-
-// // ${tests.join('')}
-// // `
-// await fs.writeFile('tests/snapshot.spec.ts', content, { encoding: 'utf-8' })
