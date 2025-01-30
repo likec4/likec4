a8402881abac24115b30e2ee743447cda5fc42ed
Denis Davydkov <denis@davydkov.com>
2025-01-30 03:38:56 +0100

    fix: exclude test files from packages

--- e2e/vitest.config.ts
+++ e2e/vitest.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    exclude: ['tests/**'],
+    include: [
+      'src/**.spec.ts',
+      'src/**.test-d.ts',
+    ],
     typecheck: {
       enabled: true,
     },
--- packages/core/src/compute-view/utils/uniqueTags.spec.ts
+++ packages/core/src/compute-view/utils/uniqueTags.spec.ts
@@ -6,7 +6,7 @@ describe('uniqueTags function', () => {
     const input = [
       { tags: ['tag1', 'tag2', 'tag3'] },
       { tags: ['tag2', 'tag3', 'tag4'] },
-      { tags: ['tag3', 'tag4', 'tag5'] }
+      { tags: ['tag3', 'tag4', 'tag5'] },
     ] as const
     const result = uniqueTags(input)
     expect(result).toEqual(['tag1', 'tag2', 'tag3', 'tag4', 'tag5'])
@@ -16,7 +16,7 @@ describe('uniqueTags function', () => {
     const input = [
       { tags: ['tag1', 'tag20', 'tag30'] },
       { tags: ['tag2', 'tag23', 'tag34'] },
-      { tags: ['tag3'] }
+      { tags: ['tag3'] },
     ] as const
     const result = uniqueTags(input)
     expect(result).toEqual([
@@ -26,7 +26,7 @@ describe('uniqueTags function', () => {
       'tag20',
       'tag23',
       'tag30',
-      'tag34'
+      'tag34',
     ])
     // ].sort(compareNatural))
   })
@@ -34,7 +34,7 @@ describe('uniqueTags function', () => {
   it('returns null if the tags array is null', () => {
     const input = [
       { tags: null },
-      {}
+      {},
     ]
     const result = uniqueTags(input)
     expect(result).toBeNull()
