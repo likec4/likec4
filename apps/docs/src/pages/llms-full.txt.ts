bdebfd6b19a061f4e0d08071662663550449dfdc
Denis Davydkov <denis@davydkov.com>
2025-03-11 13:20:32 +0100

    refactor: filter out specific imports and skip certain documents in llms API responses

--- apps/docs/src/pages/llms-full.txt.ts
+++ apps/docs/src/pages/llms-full.txt.ts
@@ -1,13 +1,27 @@
 import type { APIRoute } from 'astro'
 import { getCollection } from 'astro:content'
+import { skip } from './llms.txt'
 
 const docs = await getCollection('docs')
+
+const filterOutMdxImports = (text: string = '') => {
+  return text
+    .split('\n')
+    .filter((line) =>
+      !(
+        line.includes('import')
+        && (line.includes('@astrojs/starlight/components') || line.includes('@components/'))
+      )
+    )
+    .join('\n')
+}
 
 export const GET: APIRoute = async ({}) => {
   return new Response(
     docs
+      .filter(({ id }) => !skip.includes(id))
       .map((doc) => {
-        return `# ${doc.data.title}\n\n${doc.body}\n\n`
+        return `# ${doc.data.title}\n\n${filterOutMdxImports(doc.body)}\n\n`
       })
       .join(''),
     { headers: { 'Content-Type': 'text/plain; charset=utf-8' } },
--- apps/docs/src/pages/llms.txt.ts
+++ apps/docs/src/pages/llms.txt.ts
@@ -3,12 +3,18 @@ import { getCollection } from 'astro:content'
 
 const docs = await getCollection('docs')
 
+export const skip = [
+  '404',
+  'examples/bigbank',
+]
+
 export const GET: APIRoute = async ({ params, request }) => {
   return new Response(
     `# LikeC4 Documentation\n\n${
       docs
+        .filter(({ id }) => !skip.includes(id))
         .map((doc) => {
-          return `- [${doc.data.title}](https://likec4.dev/${doc.slug}/)\n`
+          return `- [${doc.data.title}](https://likec4.dev/${doc.id}/)\n`
         })
         .join('')
     }`,
