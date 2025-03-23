c520984744a86258fa93417e72511e5821a31e29
Denis Davydkov <denis@davydkov.com>
2025-03-23 06:26:40 +0100

    refactor: update build configuration and improve object hashing utility

--- packages/core/src/utils/object-hash.ts
+++ packages/core/src/utils/object-hash.ts
@@ -1,3 +1,14 @@
-import objectHash from 'object-hash'
+import objecthash from 'object-hash'
+import { isNonNullish } from 'remeda'
+import { invariant } from '../errors'
 
-export { objectHash }
+export function objectHash(value: any): string {
+  invariant(
+    typeof value === 'object' && isNonNullish(value),
+    'objectHash: value must be an object',
+  )
+  return objecthash(value, {
+    ignoreUnknown: true,
+    respectType: false,
+  })
+}
