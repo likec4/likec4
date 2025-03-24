eb346f674a4655d402fb02d3cc2f499de821dd60
Denis Davydkov <denis@davydkov.com>
2025-03-24 03:12:38 +0100

    refactor: update TypeScript configurations, streamline paths, and enhance build scripts across playground and packages

--- apps/playground/panda.config.ts
+++ apps/playground/panda.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@likec4/styles/dev'
 
 export default defineConfig({
   include: [
-    './src/**/*.{ts,tsx}',
+    'src/**/*.{ts,tsx}',
     '../../packages/diagram/src/**/*.{js,jsx,ts,tsx}',
   ],
 })
