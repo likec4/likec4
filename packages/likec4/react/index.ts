131326c2e0c362b0a97e373d38d768e4a9531411
Denis Davydkov <denis@davydkov.com>
2025-03-26 02:44:35 +0100

    refactor: enhance packaging scripts and streamline turbo.json configurations

--- packages/likec4/react/index.ts
+++ packages/likec4/react/index.ts
@@ -6,7 +6,6 @@ export {
   type ReactLikeC4Props,
 } from '@likec4/diagram/bundle'
 
-export { deepEqual } from 'fast-equals'
 export { createHooksForModel } from './utils-for-vite-plugin'
 
 export * as nano from './nanostores'
