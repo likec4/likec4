2d2a498c9ea95c4dfa1bc6282b4c3128a7f115cc
Denis Davydkov <denis@davydkov.com>
2025-06-27 14:51:57 +0200

    ci: add Windows check (#2013)
    Github check that runs build and tests on Windows
    It fails on checkout main branch (expected due to `aux` filename, which
    is reserved on Windows)
    
    ---------
    
    Signed-off-by: Denis Davydkov <denis@davydkov.com>

--- packages/generators/src/model/generate-likec4-model.ts
+++ packages/generators/src/model/generate-likec4-model.ts
@@ -1,32 +1,29 @@
 import type { LikeC4Model } from '@likec4/core/model'
 import JSON5 from 'json5'
-import { CompositeGeneratorNode, toString } from 'langium/generate'
 import { capitalize } from 'remeda'
 import { generateAux } from './generate-aux'
 
 export function generateLikeC4Model(model: LikeC4Model<any>) {
-  const out = new CompositeGeneratorNode()
   const aux = generateAux(model)
   const ModelData = capitalize(model.stage) + 'LikeC4ModelData'
 
-  out.appendTemplate`
-    /* prettier-ignore-start */
-    /* eslint-disable */
+  return `
+/* prettier-ignore-start */
+/* eslint-disable */
 
-    /******************************************************************************
-     * This file was generated
-     * DO NOT EDIT MANUALLY!
-     ******************************************************************************/
+/******************************************************************************
+ * This file was generated
+ * DO NOT EDIT MANUALLY!
+ ******************************************************************************/
 
-    import { LikeC4Model } from '@likec4/core/model'
-    import type { ${ModelData} } from '@likec4/core/types'
-    ${aux}
+import { LikeC4Model } from '@likec4/core/model'
+import type { ${ModelData} } from '@likec4/core/types'
+${aux}
 
-    export const likec4model: LikeC4Model<$Aux> = new LikeC4Model(<${ModelData}<$Aux>>(${
+export const likec4model: LikeC4Model<$Aux> = new LikeC4Model(<${ModelData}<$Aux>>(${
     JSON5.stringify(model.$data, { space: 2, quote: '\'' })
   } as unknown))
 
-    /* prettier-ignore-end */
-  `
-  return toString(out)
+/* prettier-ignore-end */
+`.trimStart()
 }
