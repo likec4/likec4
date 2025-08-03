846b9a4918f9c7ab2be9aae54cf81954314ec03a
Denis Davydkov <denis@davydkov.com>
2025-08-03 13:27:43 +0200

    fix(vscode): improve state management and error handling in preview panel

--- packages/vscode-preview/src/main.tsx
+++ packages/vscode-preview/src/main.tsx
@@ -1,27 +1,43 @@
-import { Box, MantineProvider, Notification } from '@mantine/core'
+import { Box, Button, Group, MantineProvider, Notification, ScrollAreaAutosize, Text } from '@mantine/core'
 import { IconX } from '@tabler/icons-react'
 import ReactDOM from 'react-dom/client'
 import { type FallbackProps, ErrorBoundary } from 'react-error-boundary'
 import { isError } from 'remeda'
 import App from './App'
-import { LikeC4Context } from './Context'
+import { stateAlert } from './App.css'
+import { refetchCurrentDiagram } from './state'
 import { theme } from './theme'
+import { ExtensionApi as extensionApi } from './vscode'
 
 const root = document.getElementById('root') as HTMLDivElement
 const scheme = document.body.classList.contains('dark') ? 'dark' : 'light'
 const nonce = root.getAttribute('nonce') || undefined
 
-const Fallback = ({ error }: FallbackProps) => {
+const Fallback = ({ error, resetErrorBoundary }: FallbackProps) => {
   return (
-    <Box p={'sm'}>
+    <Box className={stateAlert}>
       <Notification
         icon={<IconX style={{ width: 20, height: 20 }} />}
-        color={'red'}
-        style={{
-          whiteSpace: 'preserve-breaks',
+        styles={{
+          icon: {
+            alignSelf: 'flex-start',
+          },
         }}
+        color={'red'}
+        title={'Oops, something went wrong'}
         withCloseButton={false}>
-        {isError<Error>(error) ? (error.stack ?? error.message) : `Something went wrong:\n${error}`}
+        <ScrollAreaAutosize maw={'calc(100vw - 4rem)'} mah={'calc(100vh - 6rem)'}>
+          <Text
+            style={{
+              whiteSpace: 'preserve-breaks',
+            }}>
+            {error}
+          </Text>
+        </ScrollAreaAutosize>
+        <Group gap={'xs'} mt="sm">
+          <Button color="gray" variant="light" onClick={() => resetErrorBoundary()}>Reset</Button>
+          <Button color="gray" variant="subtle" onClick={extensionApi.closeMe}>Close</Button>
+        </Group>
       </Notification>
     </Box>
   )
@@ -30,9 +46,7 @@ const Fallback = ({ error }: FallbackProps) => {
 ReactDOM.createRoot(root).render(
   <MantineProvider theme={theme} forceColorScheme={scheme} {...(nonce && { getStyleNonce: () => nonce })}>
     <ErrorBoundary FallbackComponent={Fallback}>
-      <LikeC4Context>
-        <App />
-      </LikeC4Context>
+      <App />
     </ErrorBoundary>
   </MantineProvider>,
 )
