73cf99f98bab516448691e4b02884fa204ef21a8
Denis Davydkov <denis@davydkov.com>
2025-08-05 21:12:24 +0200

    refactor: remove unused edge styles and commented code

--- packages/diagram/src/base/primitives/edge/edge.css.ts
+++ packages/diagram/src/base/primitives/edge/edge.css.ts
@@ -2,31 +2,6 @@ import { css } from '@likec4/styles/css'
 
 const isSelected = '.react-flow__edge.selected'
 
-const edgeStroke = '--xy-edge-stroke'
-const edgeStrokeSelected = '--xy-edge-stroke-selected'
-const labelColor = '--xy-edge-label-color'
-const labelBg = '--xy-edge-label-background-color'
-
-export const edgeVars = css({
-  // [edgeStroke]: 'var(--likec4-palette-relation-stroke)',
-  // [edgeStrokeSelected]: 'var(--likec4-palette-relation-stroke-selected)',
-  // [labelColor]: {
-  //   base: 'var(--likec4-palette-relation-label)',
-  //   _light: `color-mix(in srgb, var(--likec4-palette-relation-label), rgba(255 255 255 / 0.85) 40%)`,
-  // },
-  // [labelBg]: {
-  //   _light: `var(--likec4-palette-relation-label-bg)/60`,
-  //   _dark: `var(--likec4-palette-relation-label-bg)/50`,
-  // },
-  // ['--xy-edge-stroke-width']: '3',
-  // '&:is([data-likec4-hovered=\'true\'],[data-edge-active=\'true\'])': {
-  //   [edgeStroke]: 'var(--likec4-palette-relation-stroke-selected)',
-  //   _whenSelected: {
-  //     ['--xy-edge-stroke-width']: '4',
-  //   },
-  // },
-})
-
 export const edgeContainer = css({
   _reduceGraphics: {
     transition: 'none',
@@ -96,40 +71,5 @@ export const cssEdgePath = css({
   _whenPanning: {
     strokeDasharray: 'none !important',
     animationName: 'none',
-  },
-})
-
-const aiBg = {
-  var: '--ai-bg',
-  ref: 'var(--ai-bg)',
-} as const
-
-export const actionBtn = css({
-  // zIndex: 'calc(var(--layer-overlays, 1) + 1)',
-  pointerEvents: 'all',
-  // color: `xyedge.label`,
-  cursor: 'pointer',
-  opacity: 0.75,
-  transition: 'fast',
-  // backgroundColor: aiBg.ref,
-  [aiBg.var]: 'var(--xy-edge-label-background-color)',
-  '--ai-hover': `color-mix(in srgb , var(--xy-edge-label-background-color), {colors.likec4.mixColor} 10%)`,
-  '--ai-size': `var(--ai-size-sm)`,
-  '--ai-radius': `var(--mantine-radius-sm)`,
-  _hover: {
-    translateY: '[1px]',
-    scale: 1.15,
-  },
-  _active: {
-    translateY: '[-1px]',
-    scale: '0.9',
-  },
-  _whenHovered: {
-    opacity: 1,
-  },
-  '& .tabler-icon': {
-    width: '80%',
-    height: '80%',
-    strokeWidth: '2',
   },
 })
