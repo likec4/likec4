f354aa4145f03e9b361a37f0100f4b8c9a40f8e2
Kieron Lanning <kieronlanning@users.noreply.github.com>
2025-08-29 20:37:53 +0100

    fix: more typescript errors

--- packages/language-server/src/model/parser/Base.ts
+++ packages/language-server/src/model/parser/Base.ts
@@ -255,17 +255,12 @@ export class BaseParser {
     const slashIndex = value.indexOf('/')
     const aliasName = slashIndex > 0 ? value.substring(0, slashIndex) : value
     const remainingPath = slashIndex > 0 ? value.substring(slashIndex + 1) : ''
-
     // Get imageAliases from project config, or use default '@' -> './images' mapping
-    const userImageAliases = this.project.config.imageAliases || {}
-    // Always have the default available, regardless of user config.
-    // They can always override it if they want.
-    const imageAliases = { '@': './images', ...userImageAliases }
+    const imageAliases: Record<string, string> = { '@': './images', ...this.project.config.imageAliases || {} }
 
     // Look up the alias path
     const aliasPath = imageAliases[aliasName]
     if (!aliasPath) {
-      // What should we do if the alias is not found?
       logger.warn(`Image alias "${aliasName}" not found in project configuration`)
       return undefined
     }
