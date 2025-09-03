107e5d1112cb2b2a4e1bd5a4fa122d3e298b922f
Denis Davydkov <denis@davydkov.com>
2025-09-03 05:10:24 +0200

    replace e2e `.likec4rc` config with `likec4.config.mjs`

--- e2e/src/likec4/likec4.config.mjs
+++ e2e/src/likec4/likec4.config.mjs
@@ -1,8 +1,10 @@
-{
+const config = {
   name: 'e2e',
   imageAliases: {
     // '@' intentionally left blank to enable the 'default' to be picked up  as ./images
     '@root': '../root-level-images',
-    '@nested': '../root-level-images/nested-1/nested-2'
-  }
+    '@nested': '../root-level-images/nested-1/nested-2',
+  },
 }
+
+export default config
