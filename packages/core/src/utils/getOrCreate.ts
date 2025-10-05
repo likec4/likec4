8f6e90fdc545ea3a89edcb52ea7c0ac6aac93124
Denis Davydkov <denis@davydkov.com>
2025-10-05 23:40:06 +0200

    refactor `getOrCreate` to use with `WeakMap`

--- packages/core/src/utils/getOrCreate.ts
+++ packages/core/src/utils/getOrCreate.ts
@@ -1,4 +1,6 @@
-export function getOrCreate<K, V>(map: Map<K, V>, key: K, create: (key: K) => V): V {
+export function getOrCreate<K, V>(map: Map<K, V>, key: K, create: (key: K) => V): V
+export function getOrCreate<K extends WeakKey, V>(map: WeakMap<K, V>, key: K, create: (key: K) => V): V
+export function getOrCreate(map: Map<any, any> | WeakMap<any, any>, key: any, create: (key: any) => any): any {
   let entry = map.get(key)
   if (!entry) {
     entry = create(key)
