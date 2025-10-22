efb7df0f33fabe76ed14b3a86faea1fff678ba54
Denis Davydkov <denis@davydkov.com>
2025-10-22 21:41:50 +0200

    refactor(migrateManualLayouts): streamline project selection and logging during migration

--- packages/vscode/src/commands/migrateManualLayouts.ts
+++ packages/vscode/src/commands/migrateManualLayouts.ts
@@ -1,5 +1,6 @@
+import { type ViewId, ProjectId } from '@likec4/core'
 import { useCommand } from 'reactive-vscode'
-import { entries, filter, isTruthy, keys, map, pipe, values } from 'remeda'
+import { filter, isTruthy, keys, map, pipe, values } from 'remeda'
 import * as vscode from 'vscode'
 import { logger, logWarn } from '../logger'
 import { commands } from '../meta'
@@ -11,7 +12,6 @@ export interface OpenPreviewCommandDeps {
 
 export function registerMigrateManualLayoutsCommand({ sendTelemetry, rpc }: OpenPreviewCommandDeps) {
   useCommand(commands.migrateManualLayouts, async () => {
-    sendTelemetry(commands.migrateManualLayouts)
     try {
       logger.info(`Migrating manual layouts started`)
       logger.debug('fetching all projects')
@@ -19,27 +19,7 @@ export function registerMigrateManualLayoutsCommand({ sendTelemetry, rpc }: Open
       let projectIds = keys(projects)
       logger.info(`Found ${projectIds.length} projects`)
 
-      if (projectIds.length > 1) {
-        const items = pipe(
-          projects,
-          entries(),
-          map(([id, p]) => ({
-            label: p.config.name,
-            description: p.folder,
-            selected: true,
-            projectId: id,
-          })),
-        )
-        const selected = await vscode.window.showQuickPick(items, {
-          canPickMany: true,
-          title: 'Select projects to migrate',
-        })
-        if (!selected || selected.length === 0) {
-          logger.info(`No projects selected, cancelling migration`)
-          return
-        }
-        projectIds = selected.map(i => i.projectId)
-      }
+      const projectsWithManualLayouts = new Map<ProjectId, ViewId[]>()
 
       for (const project of projectIds) {
         const { model } = await rpc.fetchComputedModel(project).catch(err => {
@@ -54,42 +34,81 @@ export function registerMigrateManualLayoutsCommand({ sendTelemetry, rpc }: Open
           model.views,
           values(),
           filter(v => isTruthy(v.manualLayout)),
+          map(v => v.id),
         )
         if (withManualLayouts.length === 0) {
           logger.info`No views with manual layouts in project ${project}, skipping`
           continue
         }
         logger.info`Found ${withManualLayouts.length} views with manual layouts in project ${project}`
-        for (const v of withManualLayouts) {
-          logger.debug`Layout view ${v.id} in project ${project}`
+        projectsWithManualLayouts.set(project, withManualLayouts)
+      }
+
+      if (projectsWithManualLayouts.size === 0) {
+        vscode.window.showInformationMessage(`No projects with manual layouts found, nothing to migrate`)
+        return
+      }
+      sendTelemetry(commands.migrateManualLayouts)
+
+      if (projectsWithManualLayouts.size > 1) {
+        const items = pipe(
+          [...projectsWithManualLayouts.keys()],
+          map(id => ({
+            label: projects[id]!.config.name,
+            description: projects[id]!.folder,
+            selected: true,
+            projectId: id,
+          })),
+        )
+        const selected = await vscode.window.showQuickPick(items, {
+          canPickMany: true,
+          title: 'Select projects to migrate',
+        })
+        if (!selected || selected.length === 0) {
+          logger.info(`No projects selected, cancelling migration`)
+          return
+        }
+        // remove unselected projects
+        for (const s of projectsWithManualLayouts.keys()) {
+          if (!selected.find(sel => sel.projectId === s)) {
+            projectsWithManualLayouts.delete(s)
+          }
+        }
+      }
+
+      for (const [project, withManualLayouts] of projectsWithManualLayouts.entries()) {
+        logger.debug`Migrating project ${project} with ${withManualLayouts.length} views`
+        for (const vId of withManualLayouts) {
+          logger.debug`Layout view ${vId} in project ${project}`
           const layouted = await rpc
             .layoutView({
               projectId: project,
-              viewId: v.id,
+              viewId: vId,
             })
             .catch(err => {
-              logger.warn(`Failed to layout view ${v.id} in project ${project}`, { err })
+              logger.warn(`Failed to layout view ${vId} in project ${project}`, { err })
               return null
             })
           if (!layouted) {
-            logger.warn`No layout for view ${v.id} in project ${project}, skipping`
+            logger.warn`No layout for view ${vId} in project ${project}, skipping`
             continue
           }
-          logger.info`Migrating view ${v.id} in project ${project}`
+          logger.info`Migrating view ${vId} in project ${project}`
           await rpc
             .changeView({
               projectId: project,
-              viewId: v.id,
+              viewId: vId,
               change: {
                 op: 'save-view-snapshot',
                 layout: layouted.diagram,
               },
             })
             .catch(err => {
-              logger.warn(`Failed to save manual layout for view ${v.id} in project ${project}`, { err })
+              logger.warn(`Failed to save manual layout for view ${vId} in project ${project}`, { err })
             })
         }
       }
+      vscode.window.showInformationMessage(`Migration completed`)
 
       logger.info(`Migrating manual layouts completed`)
     } catch (e) {
