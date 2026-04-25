7a0b2c4fd4f7468bf84d347a72acf9442b538730
Denis Davydkov <denis@davydkov.com>
2026-04-25 04:58:49 +0200

    shared tsdown cfg

--- devops/tsdown.ts
+++ devops/tsdown.ts
@@ -2,13 +2,14 @@ import { defu } from 'defu'
 import { type UserConfig, defineConfig as tsdownDefineConfig } from 'tsdown'
 import type { Rolldown } from 'tsdown'
 
-export function defineConfig(config: UserConfig): UserConfig {
-  return tsdownDefineConfig({
+export function defineConfig(config: UserConfig | UserConfig[]): UserConfig | UserConfig[] {
+  const base: UserConfig = {
     entry: [
       'src/**/*.{ts,tsx}',
       '!**/*.d.ts',
       '!**/*.spec.{ts,tsx}',
     ],
+    dts: true,
     outputOptions: outputOptions(),
     minify: true,
     env: {
@@ -23,26 +24,35 @@ export function defineConfig(config: UserConfig): UserConfig {
       onlyBundle: false,
     },
     ...config,
-  })
+  }
+
+  if (Array.isArray(config)) {
+    return tsdownDefineConfig(config.map(c => ({ ...base, ...c })))
+  }
+  return tsdownDefineConfig({ ...base, ...config })
 }
 
 export function outputOptions(outputOptions?: Rolldown.OutputOptions): Rolldown.OutputOptions {
-  return defu(outputOptions, {
-    keepNames: true,
-    entryFileNames: '[name].mjs',
-    chunkFileNames: 'chunks/[name].mjs',
-    codeSplitting: {
-      groups: [
-        {
-          test: /node_modules/,
-          name: (moduleId: string) => {
-            const pkgName = moduleId.match(/.*\/node_modules\/(?<package>@[^/]+\/[^/]+|[^/]+)/)
-              ?.groups?.package
-            const isDts = /\.d\.[mc]?ts$/.test(moduleId)
-            return `libs/${pkgName || 'common'}${isDts ? '.d' : ''}`
+  return defu(
+    outputOptions,
+    {
+      keepNames: true,
+      entryFileNames: '[name].mjs',
+      chunkFileNames: 'chunks/[name].mjs',
+      codeSplitting: {
+        groups: [
+          {
+            test: /node_modules/,
+            name: (moduleId: string) => {
+              const pkgName = moduleId.match(/.*\/node_modules\/(?<package>@[^/]+\/[^/]+|[^/]+)/)
+                ?.groups?.package
+              const isDts = /\.d\.[mc]?ts$/.test(moduleId)
+              return `libs/${pkgName || 'common'}${isDts ? '.d' : ''}`
+            },
+            priority: 10,
           },
-        },
-      ],
-    },
-  })
+        ],
+      },
+    } satisfies Rolldown.OutputOptions,
+  )
 }
