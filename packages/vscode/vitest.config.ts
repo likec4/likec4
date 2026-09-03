5c31fc8bafda9b8e286dbed5e2f8ccf374c18a32
Christoph Keller <ckeller@nvidia.com>
2026-09-03 15:09:28 +0200

    test(vscode): discover bootstrap icon CSP regression

---
+++ packages/vscode/vitest.config.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+import { defineVitest } from '@likec4/devops/vitest'
+
+export default defineVitest('vscode')
