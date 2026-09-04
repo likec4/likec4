53f9ecfb3f8c6a89f1fce99380a987a061e432cc
Anders Högqvist <anders.hogqvist@toyota-industries.eu>
2026-09-04 13:17:59 +0000

    Removed tail type predicate
    isBidirectional is set based on the semantic declaration of the relationship in the models, thus making the tail type irrelevant here.

--- packages/core/src/compute-view/utils/is-bidirectional-relation.ts
+++ packages/core/src/compute-view/utils/is-bidirectional-relation.ts
@@ -2,9 +2,8 @@ import type { AnyRelationshipModel } from '../../model'
 import type { AnyAux } from '../../types'
 
 /**
- * Relationship is traversable from both endpoints,
- * either because it was declared with `<->` or because it renders arrows on both ends.
+ * Relationship is traversable from both endpoints.
  */
 export function isBidirectionalRelation<A extends AnyAux>(relation: AnyRelationshipModel<A>): boolean {
-  return relation.isBidirectional || relation.tail === 'normal'
+  return relation.isBidirectional
 }
